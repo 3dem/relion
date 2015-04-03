cc5cfadd1a82338715f2d880c23a9b034a7f3d5f
Bjoern Forsberg <bjorn.forsberg@scilifelab.se>
2015-04-03 10:41:28 +0200

    Cmake handles compile on K80-machines

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -9,25 +9,32 @@
 # opted into by specifying this in CMakeLists.txt
 
 # PRECISION OPTION
-if(FFTW_SINGLE)
+if(SINGLE_RELION)
 	# set fftw lib to use single (f=float) precision
 	set(fftw "fftw3f")
-else(FFTW_SINGLE)
+else(SINGLE_RELION)
 	# set fftw lib to use double precision
 	set(fftw "fftw3")
-endif(FFTW_SINGLE)	
+endif(SINGLE_RELION)	
 
-find_library(FFTW_LIBRARIES "${fftw}")
+find_library(FFTW_LIBRARIES  NAMES "${fftw}"  PATHS $ENV{FFTW_LIB})
 
 # PARALLELISM OPTIONS
 if(NOT NOTHREAD_RELION)
-    find_library(FFTW_THREAD_LIBS "${fftw}_threads")	
+    find_library(FFTW_THREAD_LIBS  NAMES "fftw3_threads"  PATHS $ENV{FFTW_LIB})	
 	list(APPEND FFTW_LIBRARIES ${FFTW_THREAD_LIBS} )
 endif(NOT NOTHREAD_RELION)
 
-find_path(FFTW_INCLUDES ${fftw}.h)
-find_path(FFTW_PATH ${fftw}.h /usr/lib/* /usr/share/* /usr/include/* )
+message(STATUS "Looking for ${fftw}.h ...")
 
+find_path(FFTW_PATH ${fftw}.h /usr/lib/* /usr/share/* /usr/include/* $ENV{FFTW_INCLUDE} )
+find_path(FFTW_INCLUDES ${fftw}.h /usr/lib/* /usr/share/* /usr/include/* $ENV{FFTW_INCLUDE})
+#find_library(FFTW_LIBRARIES /opt/tcbsys/fftw/3.3.4-sse2-avx/lib )
+
+
+message(STATUS "FFTW_PATH: ${FFTW_PATH}")
+message(STATUS "FFTW_INCLUDES: ${FFTW_INCLUDES}")
+message(STATUS "FFTW_LIBRARIES: ${FFTW_LIBRARIES}")
 if(FFTW_PATH)
    set(FFTW_FOUND TRUE)
 endif(FFTW_PATH)
