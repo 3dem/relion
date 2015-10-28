2e9fec6495a952958b335ce450730e05826e52e9
Sjors Scheres <scheres@mrc-lmb.cam.ac.uk>
2015-10-28 12:03:41 +0000

    put back my changes in ml_optimiser_mpi.cpp and removal of persistent printf statements in src/gpu_utils/cuda_ml_optimiser.cu

--- src/ml_optimiser_mpi.cpp
+++ src/ml_optimiser_mpi.cpp
@@ -165,29 +165,42 @@ void MlOptimiserMpi::initialise()
 	}
 
 
-	if (do_gpu && !node->isMaster()) //Device not initialized on master rank
+	if (do_gpu)
 	{
-		do_shifts_onthefly = true;
-		if (!std::isdigit(*gpu_ids.begin()))
-			std::cout << " No gpu-ids specified, threads will automatically be mapped to devices (incrementally)."<< std::endl;
-		else if(gpu_ids.length()<nr_threads)
-			REPORT_ERROR("You did not supply enough gpu ids to supply all the threads you wanted");
-
-		int devCount;
-		HANDLE_ERROR(cudaGetDeviceCount(&devCount));
-		for (int i = 0; i < nr_threads; i ++)
+		// Sequential initialisation of GPUs on all ranks
+		for (int rank = 0; rank < node->size; rank++)
 		{
-			int dev_id;
-			if (!std::isdigit(*gpu_ids.begin()))
-				dev_id = i%devCount;
-			else
-				dev_id = (int)(gpu_ids[i]-'0');
+			if (rank == node->rank && !node->isMaster()) //Device not initialized on master rank
+			{
+				if (!std::isdigit(*gpu_ids.begin()))
+					std::cout << " No gpu-ids specified, threads will automatically be mapped to devices (incrementally)."<< std::endl;
+				else if(gpu_ids.length()<nr_threads)
+					REPORT_ERROR("You did not supply enough gpu ids to supply all the threads you wanted");
 
-			std::cout << " Thread " << i << " mapped to device " << dev_id << std::endl;
-			cudaMlOptimisers.push_back((void *) new MlOptimiserCuda(this, dev_id));
+				int devCount;
+				HANDLE_ERROR(cudaGetDeviceCount(&devCount));
+
+				for (int i = 0; i < nr_threads; i ++)
+				{
+					int dev_id;
+					if (!std::isdigit(*gpu_ids.begin()))
+					{
+						// Sjors: hack to make use of several cards; will only work if all MPI slaves are on the same node!
+						if (node->size > 1)
+							dev_id = ( (i * (node->size - 1)) + (node->rank - 1) )%devCount;
+						else
+							dev_id = i%devCount;
+					}
+					else
+						dev_id = (int)(gpu_ids[i]-'0');
+
+					std::cout << " Thread " << i << " on slave " << node->rank << " mapped to device " << dev_id << std::endl;
+					cudaMlOptimisers.push_back((void *) new MlOptimiserCuda(this, dev_id));
+				}
+			}
+			MPI_Barrier(MPI_COMM_WORLD);
 		}
 	}
-
 
 
 #ifdef DEBUG
@@ -792,7 +805,6 @@ void MlOptimiserMpi::expectation()
 						XFLOAT *imags = new XFLOAT[s];
 						XFLOAT *weights = new XFLOAT[s];
 
-						( (MlOptimiserCuda*) cudaMlOptimisers[i])->clearBackprojectDataBundle();
 						( (MlOptimiserCuda*) cudaMlOptimisers[i])->cudaBackprojectors[iclass].getMdlData(reals, imags, weights);
 
 						for (unsigned long n = 0; n < s; n++)
