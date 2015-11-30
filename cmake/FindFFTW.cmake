34a9f40eb8c5a439626fddedbd394181ef282103
Dari Kimanius <dkimanius@gmail.com>
2015-11-30 17:02:11 +0100

    fftw package finder checks the require flag, required by the cmake standard. Also if one of the paths are not found fftw_found is not set to true. Only the required library are included in the lib-path instead of all three (single, double and thread). This is better in case one does not have all of them installed. Besides relion doesn't uses the thread enabled version anymore.

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -17,33 +17,11 @@ else(DoublePrec_CPU)
 	set(fft "fftw3f")
 endif(DoublePrec_CPU)	
 
+set(LIB_PATHFFT $ENV{FFTW_LIB})
+set(INC_PATHFFT $ENV{FFTW_INCLUDE})
 
-#set(USE_CUFFT FALSE)
-if(CUFFT)
-    set(LIB_PATHFFT $ENV{CUDA_HOME}/lib64)
-    set(INC_PATHFFT $ENV{CUDA_HOME}/include)
-    find_library(FFTW_LIBRARIES  NAMES cufftw PATHS ${LIB_PATHFFT})
-    find_library(FFT_LIBRARIES   NAMES cufft  PATHS ${LIB_PATHFFT})
-    list(APPEND FFTW_LIBRARIES ${FFT_LIBRARIES} )
-    set(fft "cufft")
-    
-else(CUFFT)
-    set(LIB_PATHFFT $ENV{FFTW_LIB})
-    set(INC_PATHFFT $ENV{FFTW_INCLUDE})
- 
-    find_library(FFTW_LIBRARIES  NAMES fftw3f  PATHS ${LIB_PATHFFT})  
-    find_library(FFTWD_LIBRARIES   NAMES fftw3   PATHS ${LIB_PATHFFT}) 
-    list(APPEND FFTW_LIBRARIES ${FFTWD_LIBRARIES} )
-    # PARALLELISM OPTIONS
+find_library(FFTW_LIBRARIES  NAMES ${fft}  PATHS ${LIB_PATHFFT})
 
-    if(NOT NOTHREAD_RELION)
-        find_library(FFTW_THREAD_LIBS  NAMES "fftw3_threads"  PATHS $ENV{FFTW_LIB})	
-    	list(APPEND FFTW_LIBRARIES ${FFTW_THREAD_LIBS} )
-    endif(NOT NOTHREAD_RELION)
-   
-endif(CUFFT)    
-
-message(STATUS "Looking for ${fft}.h ...")
 if(DEFINED ENV{FFTW_INCLUDE})
     find_path(FFTW_PATH     NAMES fftw3.h  PATHS ${INC_PATHFFT} NO_DEFAULT_PATH)
     find_path(FFTW_INCLUDES NAMES fftw3.h  PATHS ${INC_PATHFFT} NO_DEFAULT_PATH)
@@ -51,13 +29,19 @@ else()
     find_path(FFTW_PATH         NAMES fftw3.h )
     find_path(FFTW_INCLUDES     NAMES fftw3.h )
 endif()
-    
-
-#find_library(FFTW_LIBRARIES /opt/tcbsys/fftw/3.3.4-sse2-avx/lib )
 
 message(STATUS "FFTW_PATH: ${FFTW_PATH}")
 message(STATUS "FFTW_INCLUDES: ${FFTW_INCLUDES}")
 message(STATUS "FFTW_LIBRARIES: ${FFTW_LIBRARIES}")
-if(FFTW_PATH)
+
+if(FFTW_PATH AND FFTW_INCLUDES AND FFTW_LIBRARIES)
+	message(STATUS "${fft} found.")
    set(FFTW_FOUND TRUE)
-endif(FFTW_PATH)
+else(FFTW_PATH AND FFTW_INCLUDES AND FFTW_LIBRARIES)
+	message(STATUS "${fft} not found.")
+   set(FFTW_FOUND FALSE)
+endif(FFTW_PATH AND FFTW_INCLUDES AND FFTW_LIBRARIES)
+
+if(FFTW_FIND_REQUIRED AND NOT FFTW_FOUND)
+	message( FATAL_ERROR "FFTW package is required." )
+endif(FFTW_FIND_REQUIRED AND NOT FFTW_FOUND)
