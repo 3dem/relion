1e607c867f6fc59ff7be21c10a75a2e880ab3f78
Bjoern Forsberg <bjorn.forsberg@scilifelab.se>
2016-06-16 18:15:25 +0200

    temp non-working state for merge

--- src/apps/autopick_mpi.cpp
+++ src/apps/autopick_mpi.cpp
@@ -32,19 +32,22 @@ int main(int argc, char *argv[])
 
 		prm.initialise();
 
-#ifdef CUDA
-		if (prm.do_gpu)
+		if (prm.todo_anything)
 		{
-			std::stringstream didSs;
-			didSs << "APr" << prm.getRank();
-			int dev_id = prm.deviceInitialise();
-			prm.cudaPicker = (void*) new AutoPickerCuda((AutoPickerMpi*)&prm, dev_id, didSs.str().c_str() );
+#ifdef CUDA
+			if (prm.do_gpu)
+			{
+				std::stringstream didSs;
+				didSs << "APr" << prm.getRank();
+				int dev_id = prm.deviceInitialise();
+				prm.cudaPicker = (void*) new AutoPickerCuda((AutoPicker*)&prm, dev_id, didSs.str().c_str() );
 
-			((AutoPickerCuda*)prm.cudaPicker)->run();
-		}
-		else
+				((AutoPickerCuda*)prm.cudaPicker)->run();
+			}
+			else
 #endif
-			prm.run();
+				prm.run();
+		}
     }
 
     catch (RelionError XE)
