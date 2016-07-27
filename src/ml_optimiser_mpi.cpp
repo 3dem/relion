b95e8d0440d57ff587c9436d1aa22c5d74264e6d
Bjoern Forsberg <bjorn.forsberg@scilifelab.se>
2016-07-27 10:08:50 +0200

    Fix to subset during split random halves

--- src/ml_optimiser_mpi.cpp
+++ src/ml_optimiser_mpi.cpp
@@ -990,6 +990,10 @@ void MlOptimiserMpi::expectation()
 						JOB_FIRST = mydata.numberOfOriginalParticles(1) + nr_ori_particles_done_subset2;
 						JOB_LAST  = XMIPP_MIN(mydata.numberOfOriginalParticles() - 1, JOB_FIRST + nr_pool - 1);
 					}
+					if(iter>subset_iter)
+						nr_particles_todo = mydata.numberOfOriginalParticles(random_subset);
+					else
+						nr_particles_todo = (double)(mydata.numberOfOriginalParticles(random_subset))*subset_frac;
 				}
 				else
 				{
