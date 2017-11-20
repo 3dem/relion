ede23e082b101f9df6abe25c4dea2621de7f2249
Charles Congdon <charles.w.congdon@intel.com>
2017-11-20 15:04:48 -0800

    CUDA build should not use TBB

--- src/acc/acc_backprojector.h
+++ src/acc/acc_backprojector.h
@@ -2,13 +2,15 @@
 #define ACC_BACKPROJECTOR_H_
 
 #ifdef CUDA
-#include <cuda_runtime.h>
+#  include <cuda_runtime.h>
 #endif
 #include "src/complex.h"
 #include "src/acc/settings.h"
 #include "src/acc/acc_ptr.h"
 
-#include <tbb/spin_mutex.h>
+#ifndef CUDA
+#  include <tbb/spin_mutex.h>
+#endif
 
 class AccBackprojector
 {
@@ -19,7 +21,9 @@ public:
 	    maxR, maxR2,
 	    padding_factor;
 
+#ifndef CUDA
 	tbb::spin_mutex *mutexes;
+#endif
 
 	size_t allocaton_size;
 	int voxelCount;
@@ -37,8 +41,10 @@ public:
 				padding_factor(0),
 				allocaton_size(0), voxelCount(0),
 				d_mdlReal(NULL), d_mdlImag(NULL), d_mdlWeight(NULL),
-				stream(0),
-				mutexes(0)
+				stream(0)
+#ifndef CUDA
+				, mutexes(0)
+#endif
 	{}
 
 	size_t setMdlDim(
