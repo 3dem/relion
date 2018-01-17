388fa0547b75d7d8177d230f2ad38acd2a07c2a7
Dari Kimanius <dkimanius@gmail.com>
2018-01-17 20:27:54 +0100

    Fixed issue #132, both single and double precision FFTW libs are actually built when required simultaneously.

--- cmake/BuildFFTW.cmake
+++ cmake/BuildFFTW.cmake
@@ -5,22 +5,22 @@ if (NOT DEFINED TARGET_X86)
                 "${CMAKE_SOURCE_DIR}/cmake/TestX86.c")
 endif()
 
-find_path(   FFTW_INCLUDES NAMES fftw3.h PATHS ${FFTW_EXTERNAL_PATH}/include NO_DEFAULT_PATH) 
-find_library(FFTW_SINGLE   NAMES fftw3   PATHS ${FFTW_EXTERNAL_PATH}/lib     NO_DEFAULT_PATH)
-find_library(FFTW_DOUBLE   NAMES fftw3f  PATHS ${FFTW_EXTERNAL_PATH}/lib     NO_DEFAULT_PATH)
+find_path(   OWN_FFTW_INCLUDES NAMES fftw3.h PATHS ${FFTW_EXTERNAL_PATH}/include NO_DEFAULT_PATH) 
+find_library(OWN_FFTW_SINGLE   NAMES fftw3   PATHS ${FFTW_EXTERNAL_PATH}/lib     NO_DEFAULT_PATH)
+find_library(OWN_FFTW_DOUBLE   NAMES fftw3f  PATHS ${FFTW_EXTERNAL_PATH}/lib     NO_DEFAULT_PATH)
 
-if(FFTW_INCLUDES AND (FFTW_SINGLE OR NOT FFTW_SINGLE_REQUIRED) AND (FFTW_DOUBLE OR NOT FFTW_DOUBLE_REQUIRED))
+if(OWN_FFTW_INCLUDES AND (OWN_FFTW_SINGLE OR NOT FFTW_SINGLE_REQUIRED) AND (OWN_FFTW_DOUBLE OR NOT FFTW_DOUBLE_REQUIRED))
 
 	message(STATUS "Found previously built non-system FFTW libraries that will be used.")
 
 	set(FFTW_FOUND TRUE)
 	set(BUILD_OWN_FFTW FALSE)
-	set(FFTW_LIBRARIES ${FFTW_SINGLE} ${FFTW_DOUBLE})
+	set(BUILD_OWN_FFTWF FALSE)
 	
 else()
 	
 	message(STATUS "--------------------------------------------------------")
-	message(STATUS "-------- NO EXISTING FFTW LIBRARIES WHERE FOUND. -------")
+	message(STATUS "------ REQUIRED FFTW LIBRARIES WHERE NOT FOUND. --------")
 	message(STATUS "-------------- FFTW WILL BE DOWNLOADED AND -------------")
 	message(STATUS "--------------- BUILT DURING COMPILE-TIME. -------------")
 	message(STATUS "--------------------------------------------------------")
@@ -28,24 +28,20 @@ else()
 	message(STATUS "--------------------------------------------------------")
 	
 	set(FFTW_FOUND FALSE)
-	set(BUILD_OWN_FFTW TRUE)
 	
-	set(ext_conf_flags_fft --enable-shared --enable-float --prefix=${FFTW_EXTERNAL_PATH})
+	set(ext_conf_flags_fft --enable-shared --prefix=${FFTW_EXTERNAL_PATH})
 	if(TARGET_X86)
-		set(ext_conf_flags_fft ${ext_conf_flags_fft} --enable-sse --enable-avx)
+		set(ext_conf_flags_fft ${ext_conf_flags_fft} --enable-avx)
 	endif()
 	
-	set(FFTW_SINGLE ${FFTW_EXTERNAL_PATH}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}fftw3${CMAKE_SHARED_LIBRARY_SUFFIX})
-	set(FFTW_DOUBLE ${FFTW_EXTERNAL_PATH}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}fftw3f${CMAKE_SHARED_LIBRARY_SUFFIX})
-	set(FFTW_LIBRARIES ${FFTW_SINGLE} ${FFTW_DOUBLE})
+	set(OWN_FFTW_SINGLE ${FFTW_EXTERNAL_PATH}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}fftw3${CMAKE_SHARED_LIBRARY_SUFFIX})
+	set(OWN_FFTW_DOUBLE ${FFTW_EXTERNAL_PATH}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}fftw3f${CMAKE_SHARED_LIBRARY_SUFFIX})
+	set(OWN_FFTW_INCLUDES "${FFTW_EXTERNAL_PATH}/include" )
 	
-	set(FFTW_PATH      "${FFTW_EXTERNAL_PATH}" )
-	set(FFTW_INCLUDES  "${FFTW_EXTERNAL_PATH}/include" )
+	set(FFTW_PATH ${FFTW_PATH} ${FFTW_EXTERNAL_PATH})
 
-	include(ExternalProject)
 	set(FFTW_EXTERNAL_LIBS_TAR_DIRECTORY  ${FFTW_EXTERNAL_PATH})
 	set(FFTW_EXTERNAL_LIBS_EXTRACT_TARGET ${FFTW_EXTERNAL_LIBS_TAR_DIRECTORY})
-	message( STATUS "no previous fftw found, the following paths are set for libs/headers TO BE built")
 
 	set(FFTW_FFTW3_TAR_FILE ftp://ftp.fftw.org/pub/fftw/fftw-3.3.4.tar.gz)
 	#set(FFTW_FFTW3_TAR_FILE https://drive.google.com/uc?export=download&id=0B942d76zVnSeazZWcExRaXIyVDg) #backup location fftw-3.3.4
@@ -54,8 +50,21 @@ else()
 	set(FFTW_FFTW3_LIB_DIR ${FFTW_EXTERNAL_LIBS_EXTRACT_TARGET}/fftw3)
 	set(FFTW_FFTW3_BUILD_DIR ${FFTW_EXTERNAL_LIBS_EXTRACT_TARGET}/fftw3-build)
 
-	#set(CMAKE_INSTALL_PREFIX  ${FFTW_EXTERNAL_PATH})
-	externalproject_add(OWN_FFTW
+	include(ExternalProject)
+	
+	if (FFTW_SINGLE_REQUIRED)
+		set(BUILD_OWN_FFTW TRUE)
+	endif()
+	
+	if (FFTW_DOUBLE_REQUIRED)
+		set(BUILD_OWN_FFTWF TRUE)
+	endif()
+	
+	if (NOT FFTW_DOUBLE_REQUIRED)
+		set(ext_conf_flags_fft ${ext_conf_flags_fft} --enable-float --enable-sse)
+	endif()
+	
+	externalproject_add(own_fftw_lib
 	URL ${FFTW_FFTW3_TAR_FILE}
 	URL_MD5 2edab8c06b24feeb3b82bbb3ebf3e7b3
 	DOWNLOAD_DIR ${FFTW_EXTERNAL_LIBS_TAR_DIRECTORY}
@@ -67,8 +76,37 @@ else()
 	#	LOG_CONFIGURE
 	#	LOG_BUILD
 	LOG_INSTALL)
+	
+	
+	if (FFTW_DOUBLE_REQUIRED AND FFTW_SINGLE_REQUIRED)
+		
+		externalproject_add(own_fftwf_lib
+		URL ${FFTW_FFTW3_TAR_FILE}
+		URL_MD5 2edab8c06b24feeb3b82bbb3ebf3e7b3
+		DOWNLOAD_DIR ${FFTW_EXTERNAL_LIBS_TAR_DIRECTORY}
+		SOURCE_DIR ${FFTW_FFTW3_LIB_DIR}
+		CONFIGURE_COMMAND <SOURCE_DIR>/configure ${ext_conf_flags_fft}  --enable-float --enable-sse
+		INSTALL_DIR ${FFTW_EXTERNAL_PATH}/fftw3
+		BINARY_DIR ${FFTW_EXTERNAL_PATH}/fftw3
+		BUILD_COMMAND ${MAKE}
+		#	LOG_CONFIGURE
+		#	LOG_BUILD
+		LOG_INSTALL)
+	
+		add_dependencies(own_fftwf_lib own_fftw_lib)
+	endif()
 
 endif()
 
-message(STATUS "FFTW_INCLUDES:     ${FFTW_INCLUDES}")
-message(STATUS "FFTW_LIBRARIES:    ${FFTW_LIBRARIES}")
+if (FFTW_SINGLE_REQUIRED)
+	set(FFTW_LIBRARIES ${OWN_FFTW_SINGLE} ${FFTW_LIBRARIES})
+endif()
+
+if (FFTW_DOUBLE_REQUIRED)
+	set(FFTW_LIBRARIES ${OWN_FFTW_DOUBLE} ${FFTW_LIBRARIES})
+endif()
+
+set(FFTW_INCLUDES ${OWN_FFTW_INCLUDES} ${FFTW_INCLUDES})
+
+#message(STATUS "FFTW_INCLUDES:     ${FFTW_INCLUDES}")
+#message(STATUS "FFTW_LIBRARIES:    ${FFTW_LIBRARIES}")
