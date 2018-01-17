30848d5c70e4ff35c2e50be7f7a6d0342177ee89
Takanori Nakane <nakane.t@gmail.com>
2018-01-17 14:55:59 +0000

    Started working on Micrograph class.

---
+++ src/micrograph_model.cpp
@@ -0,0 +1,151 @@
+/***************************************************************************
+ *
+ * Author: "Sjors H.W. Scheres", "Takanori Nakane"
+ * MRC Laboratory of Molecular Biology
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This complete copyright notice must be included in any revised version of the
+ * source code. Additional authorship citations may be added, but existing
+ * author citations must be preserved.
+ ***************************************************************************/
+
+#include "src/micrograph_model.h"
+#include "src/metadata_table.h"
+#include "src/image.h"
+
+/* Work in progress!
+ *
+ * Refactor relion_preprocess (windowing) & particle_polished (weighted sum) 
+ * Should we allow extrapolation? Probably not.
+ * Use factory pattern?
+ * Where is the origin? Recent versions of MotionCor2 use the MIDDLE frame, not the first frame.
+ */
+
+void Micrograph::setMovie(FileName fnMovie, FileName fnGain) {
+	Image<RFLOAT> Ihead;
+	Ihead.read(fnMovie, false);
+        
+	width = XSIZE(Ihead());
+	height = YSIZE(Ihead());
+	nFrame = NSIZE(Ihead());
+
+	globalShiftX.resize(nFrame, 0);
+	globalShiftY.resize(nFrame, 0);
+
+	this->fnMovie = fnMovie;
+	this->fnGain = fnGain;
+}
+
+// Read from a STAR file
+void Micrograph::read(FileName fn_in)
+{
+	// Clear current model
+	clear();
+
+	// Open input file
+	std::ifstream in(fn_in.data(), std::ios_base::in);
+	if (in.fail()) {
+		REPORT_ERROR( (std::string) "MicrographModel::read: File " + fn_in + " cannot be read." );
+	}
+
+	MetaDataTable MDglobal;
+
+	// Read Image metadata
+	MDglobal.readStar(in, "general");
+
+	if (!MDglobal.getValue(EMDL_IMAGE_SIZEX, width) ||
+	    !MDglobal.getValue(EMDL_IMAGE_SIZEY, height) ||
+	    !MDglobal.getValue(EMDL_IMAGE_SIZEZ, nFrame) ||
+	    !MDglobal.getValue(EMDL_MICROGRAPH_MOVIE_NAME, fnMovie)) {
+		REPORT_ERROR("MicrographModel::read: insufficient general information");
+	}
+
+	globalShiftX.resize(nFrame, 0);
+	globalShiftY.resize(nFrame, 0);
+
+	// TOOD: read gain
+	
+	// Read global shifts
+	int frame;
+	RFLOAT shiftX, shiftY;
+
+	MDglobal.readStar(in, "global_shift");
+
+	FOR_ALL_OBJECTS_IN_METADATA_TABLE(MDglobal)
+	{
+		if (!MDglobal.getValue(EMDL_MICROGRAPH_FRAME_NUMBER, frame) ||
+		    !MDglobal.getValue(EMDL_MICROGRAPH_SHIFT_X, shiftX) ||
+		    !MDglobal.getValue(EMDL_MICROGRAPH_SHIFT_Y, shiftY)) { 
+			REPORT_ERROR("MicrographModel::read: incorrect global_shift table");
+		}
+
+		// frame is 1-indexed!
+		globalShiftX[frame - 1] = shiftX;
+		globalShiftY[frame - 1] = shiftY;
+		std::cout << " global shift: frame #" << frame << " x " << shiftX << " Y " << shiftY << std::endl;
+	}
+
+	model = new MotionModel();	
+}
+
+// Write to a STAR file
+void Micrograph::write(FileName filename) {
+	std::ofstream fh;
+	MetaDataTable MD;
+
+	fh.open(filename.c_str());
+	if (!fh) {
+		REPORT_ERROR((std::string)"Micrograph::write: Cannot write file: " + filename);
+	}
+
+        MD.setName("general");
+        MD.setIsList(true);
+        MD.addObject();
+	MD.setValue(EMDL_IMAGE_SIZEX, width);
+        MD.setValue(EMDL_IMAGE_SIZEY, height);
+        MD.setValue(EMDL_IMAGE_SIZEZ, nFrame);
+        MD.setValue(EMDL_MICROGRAPH_MOVIE_NAME, fnMovie);
+	MD.write(fh);
+
+	MD.clear();
+	MD.setName("global_shift");
+	for (int frame = 0; frame < nFrame; frame++) {
+		MD.addObject();
+		MD.setValue(EMDL_MICROGRAPH_FRAME_NUMBER, frame + 1); // make 1-indexed
+		MD.setValue(EMDL_MICROGRAPH_SHIFT_X, globalShiftX[frame]);
+		MD.setValue(EMDL_MICROGRAPH_SHIFT_Y, globalShiftY[frame]);
+	}
+	MD.write(fh);
+
+	fh.close();
+}
+
+// Get motion at frame and (x, y)
+int Micrograph::getShiftAt(int frame, RFLOAT x, RFLOAT y, RFLOAT *shiftx, RFLOAT *shifty) {
+	model->getShiftAt(frame, x, y, shiftx, shifty);
+
+	// frame is 1-indexed!
+	*shiftx += globalShiftX[frame - 1];
+	*shifty += globalShiftY[frame - 1];
+}
+
+// TODO: Implement
+int MotionModel::getShiftAt(int frame, RFLOAT x, RFLOAT y, RFLOAT *shiftx, RFLOAT *shifty) {
+	*shiftx = 0.0;
+	*shifty = 0.0;
+
+	return 0;
+}
+
+void MotionModel::fit() {
+	REPORT_ERROR("MotionModel::fit() Not implemented!");
+}
---
+++ src/micrograph_model.h
@@ -0,0 +1,94 @@
+/***************************************************************************
+ *
+ * Author: "Sjors H.W. Scheres", "Takanori Nakane"
+ * MRC Laboratory of Molecular Biology
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This complete copyright notice must be included in any revised version of the
+ * source code. Additional authorship citations may be added, but existing
+ * author citations must be preserved.
+ ***************************************************************************/
+#ifndef MICROGRAPH_MODEL_H_
+#define MICROGRAPH_MODEL_H_
+
+#include <vector>
+#include "src/filename.h"
+
+class MotionModel
+{
+public:
+	// Fit model based on observations
+	virtual void fit();
+
+	// Get motion at frame and (x, y)
+	virtual int getShiftAt(int frame, RFLOAT x, RFLOAT y, RFLOAT *shiftx, RFLOAT *shifty);
+};
+
+class Micrograph
+{
+public:
+	// Empty Constructor is not allowed
+	Micrograph(); // = delete in C++11
+
+	// Create from a movie or a STAR file
+	Micrograph(FileName filename, FileName fnGain="") {
+		clear();
+
+		if (filename.getExtension() == "star" && fnGain == "") {
+			read(filename);
+		} else {
+			setMovie(filename, fnGain);
+		}
+	}
+
+	~Micrograph()
+	{
+		clear();
+	}
+
+	// Initialise
+	void clear()
+	{
+		width = 0;
+		height = 0;
+		nFrame = 0;
+
+		fnMovie = "";
+		fnGain = "";
+
+		globalShiftX.resize(0);
+		globalShiftY.resize(0);
+	
+		if (model != NULL) delete model;
+		model = NULL;
+	}
+
+	// Read micrograph model from a STAR file
+	void read(FileName filename);
+	// Write micrograph model from a STAR file
+	void write(FileName filename);
+	// Set target movie file
+	void setMovie(FileName fnMovie, FileName fnGain="");
+
+	// Get shift vector at (x, y, frame)
+	int getShiftAt(int frame, RFLOAT x, RFLOAT y, RFLOAT *shiftx, RFLOAT *shifty);
+
+private:
+	int width, height, nFrame;
+	FileName fnGain;
+	FileName fnMovie;
+	
+	std::vector<RFLOAT> globalShiftX, globalShiftY, coeffs;
+	MotionModel *model;
+};
+
+#endif /* MICROGRAPH_MODEL_H_ */
