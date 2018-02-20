af8ce3c3821aa1922037de0a1d021cd608e84b0b
Jasenko Zivanov <jzivanov@mrc-lmb.cam.ac.uk>
2018-02-20 21:09:14 +0100

    improved aberration plot

--- src/apps/ref_aberration_plot.cpp
+++ src/apps/ref_aberration_plot.cpp
@@ -75,82 +75,193 @@ int AberrationPlot::_run()
 {
     std::vector<ParFourierTransformer> fts(nr_omp_threads);
 
-    std::vector<Image<double>>
-            A(nr_omp_threads, Image<double>(sh,s)),
-            b(nr_omp_threads, Image<double>(sh,s));
-
-    const double as = (double)s * angpix;
-
     double t0 = omp_get_wtime();
 
-    for (long g = minMG; g <= gc; g++)
+    const bool differential = false;
+
+    if (differential)
     {
-        std::stringstream stsg;
-        stsg << g;
+        std::vector<Image<double>>
+                A(nr_omp_threads, Image<double>(sh,s)),
+                b(nr_omp_threads, Image<double>(sh,s));
 
-        std::cout << "micrograph " << g << " / " << mdts.size() <<"\n";
+        const double as = (double)s * angpix;
 
-        const int pc = mdts[g].numberOfObjects();
-
-        std::vector<Image<Complex> > pred;
-        std::vector<Image<Complex> > obsF;
-
-        pred = obsModel.predictObservations(projectors[0], mdts[g], false, true, nr_omp_threads);
-        obsF = StackHelper::loadStackFS(&mdts[g], imgPath, nr_omp_threads, &fts);
-
-        #pragma omp parallel for num_threads(nr_omp_threads)
-        for (long p = 0; p < pc; p++)
+        for (long g = minMG; g <= gc; g++)
         {
-            int t = omp_get_thread_num();
+            std::stringstream stsg;
+            stsg << g;
 
-            CTF ctf0;
-            ctf0.read(mdts[g], mdts[g], p);
-            //ctf0.Cs = 0.0;
-            ctf0.initialise();
+            std::cout << "micrograph " << g << " / " << mdts.size() <<"\n";
 
+            const int pc = mdts[g].numberOfObjects();
+
+            std::vector<Image<Complex> > pred;
+            std::vector<Image<Complex> > obsF;
+
+            pred = obsModel.predictObservations(projectors[0], mdts[g], false, true, nr_omp_threads);
+            obsF = StackHelper::loadStackFS(&mdts[g], imgPath, nr_omp_threads, &fts);
+
+            #pragma omp parallel for num_threads(nr_omp_threads)
+            for (long p = 0; p < pc; p++)
+            {
+                int t = omp_get_thread_num();
+
+                CTF ctf0;
+                ctf0.read(mdts[g], mdts[g], p);
+                //ctf0.Cs = 0.0;
+                ctf0.initialise();
+
+                for (int y = 0; y < s;  y++)
+                for (int x = 0; x < sh; x++)
+                {
+                    const double xf = x;
+                    const double yf = y < sh? y : y - s;
+                    const double gamma_i = ctf0.getGamma(xf/as, yf/as);
+                    const double cg = cos(gamma_i);
+                    const double sg = sin(gamma_i);
+
+                    Complex zobs = obsF[p](y,x);
+                    Complex zprd = pred[p](y,x);
+
+                    double zz = zobs.real*zprd.real + zobs.imag*zprd.imag;
+                    double nr = zprd.norm();
+
+                    A[t](y,x) += nr*cg*cg;
+                    b[t](y,x) += cg*(sg*nr+zz);
+                }
+            }
+        }
+
+        for (int t = 1; t < nr_omp_threads; t++)
+        {
             for (int y = 0; y < s;  y++)
             for (int x = 0; x < sh; x++)
             {
-                const double xf = x;
-                const double yf = y < sh? y : y - s;
-                const double gamma_i = ctf0.getGamma(xf/as, yf/as);
-                const double cg = cos(gamma_i);
-                const double sg = sin(gamma_i);
-
-                Complex zobs = obsF[p](y,x);
-                Complex zprd = pred[p](y,x);
-
-                double zz = zobs.real*zprd.real + zobs.imag*zprd.imag;
-                double nr = zprd.norm();
-
-                A[t](y,x) += nr*cg*cg;
-                b[t](y,x) += cg*(sg*nr+zz);
+                A[0](y,x) += A[t](y,x);
+                b[0](y,x) += b[t](y,x);
             }
         }
-    }
 
-    for (int t = 1; t < nr_omp_threads; t++)
-    {
+        Image<RFLOAT> dgamma(sh,s);
+
         for (int y = 0; y < s;  y++)
         for (int x = 0; x < sh; x++)
         {
-            A[0](y,x) += A[t](y,x);
-            b[0](y,x) += b[t](y,x);
+            if (A[0](y,x) != 0.0)
+            {
+                dgamma(y,x) = b[0](y,x)/A[0](y,x);
+            }
         }
+
+        VtkHelper::writeVTK(dgamma, outPath+"_dgamma.vtk");
     }
-
-    Image<RFLOAT> dgamma(sh,s);
-
-    for (int y = 0; y < s;  y++)
-    for (int x = 0; x < sh; x++)
+    else
     {
-        if (A[0](y,x) != 0.0)
-        {
-            dgamma(y,x) = b[0](y,x)/A[0](y,x);
-        }
-    }
+        std::vector<Image<double>>
+            Axx(nr_omp_threads, Image<double>(sh,s)),
+            Axy(nr_omp_threads, Image<double>(sh,s)),
+            Ayy(nr_omp_threads, Image<double>(sh,s)),
+            bx(nr_omp_threads, Image<double>(sh,s)),
+            by(nr_omp_threads, Image<double>(sh,s));
 
-    VtkHelper::writeVTK(dgamma, outPath+"_dgamma.vtk");
+        const double as = (double)s * angpix;
+
+        for (long g = minMG; g <= gc; g++)
+        {
+            std::stringstream stsg;
+            stsg << g;
+
+            std::cout << "micrograph " << g << " / " << mdts.size() <<"\n";
+
+            const int pc = mdts[g].numberOfObjects();
+
+            std::vector<Image<Complex> > pred;
+            std::vector<Image<Complex> > obsF;
+
+            pred = obsModel.predictObservations(projectors[0], mdts[g], false, true, nr_omp_threads);
+            obsF = StackHelper::loadStackFS(&mdts[g], imgPath, nr_omp_threads, &fts);
+
+            #pragma omp parallel for num_threads(nr_omp_threads)
+            for (long p = 0; p < pc; p++)
+            {
+                int t = omp_get_thread_num();
+
+                CTF ctf0;
+                ctf0.read(mdts[g], mdts[g], p);
+                //ctf0.Cs = 0.0;
+                ctf0.initialise();
+
+                for (int y = 0; y < s;  y++)
+                for (int x = 0; x < sh; x++)
+                {
+                    const double xf = x;
+                    const double yf = y < sh? y : y - s;
+                    const double gamma_i = ctf0.getGamma(xf/as, yf/as);
+                    const double cg = cos(gamma_i);
+                    const double sg = sin(gamma_i);
+
+                    Complex zobs = obsF[p](y,x);
+                    Complex zprd = pred[p](y,x);
+
+                    double zz = zobs.real*zprd.real + zobs.imag*zprd.imag;
+                    double nr = zprd.norm();
+
+                    Axx[t](y,x) += nr*sg*sg;
+                    Axy[t](y,x) += nr*cg*sg;
+                    Ayy[t](y,x) += nr*cg*cg;
+
+                    bx[t](y,x) += zz*sg;
+                    by[t](y,x) += zz*cg;
+                }
+            }
+        }
+
+        for (int t = 1; t < nr_omp_threads; t++)
+        {
+            for (int y = 0; y < s;  y++)
+            for (int x = 0; x < sh; x++)
+            {
+                Axx[0](y,x) += Axx[t](y,x);
+                Axy[0](y,x) += Axy[t](y,x);
+                Ayy[0](y,x) += Ayy[t](y,x);
+
+                bx[0](y,x) += bx[t](y,x);
+                by[0](y,x) += by[t](y,x);
+            }
+        }
+
+        Image<RFLOAT> xx(sh,s), xy(sh,s), phase(sh,s);
+
+        for (int y = 0; y < s;  y++)
+        for (int x = 0; x < sh; x++)
+        {
+            d2Matrix A(
+                Axx[0](y,x), Axy[0](y,x),
+                Axy[0](y,x), Ayy[0](y,x));
+
+            d2Vector b(bx[0](y,x), by[0](y,x));
+
+            double det = A(0,0)*A(1,1) - A(1,0)*A(0,1);
+
+            if (det != 0.0)
+            {
+                d2Matrix Ai = A;
+                Ai.invert();
+
+                d2Vector opt = Ai*b;
+
+                xx(y,x) = opt.x;
+                xy(y,x) = opt.y;
+
+                phase(y,x) = std::abs(opt.x) > 0.0? atan2(opt.y, opt.x) : 0.0;
+            }
+        }
+
+        VtkHelper::writeVTK(xx, outPath+"_cos.vtk");
+        VtkHelper::writeVTK(xy, outPath+"_sin.vtk");
+        VtkHelper::writeVTK(phase, outPath+"_phase.vtk");
+    }
 
     return 0;
 }
