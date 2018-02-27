3dem/relion

commit 14e38ccb2fa45d25d4dc8cc25a552f4e1f2176cc

Clean-up Image

src/acc/data_types.h
@@ -20,64 +20,31 @@ public:
 				         CONSTRUCTORS
 	======================================================*/
 
-	Image(CudaCustomAllocator *allocator):
-		AccPtr<T>(allocator),
+	Image(AccPtrFactory &f):
+		AccPtr<T>(f.make<T>()),
 		x(0), y(0), z(0), fourier(false)
 	{}
 
-	Image(cudaStream_t stream, CudaCustomAllocator *allocator):
-		AccPtr<T>(stream, allocator),
-		x(0), y(0), z(0), fourier(false)
-	{}
-
-	Image(int xdim, int ydim, CudaCustomAllocator *allocator):
-		AccPtr<T>(xdim*ydim, allocator),
+	Image(int xdim, int ydim, AccPtrFactory &f):
+		AccPtr<T>(f.make<T>(xdim*ydim)),
 		x(xdim), y(ydim), z(1), fourier(false)
 	{}
 
-	Image(int xdim, int ydim, int zdim, CudaCustomAllocator *allocator):
-		AccPtr<T>(xdim*ydim*zdim, allocator),
+	Image(int xdim, int ydim, int zdim, AccPtrFactory &f):
+		AccPtr<T>(f.make<T>(xdim*ydim*zdim)),
 		x(xdim), y(ydim), z(zdim), fourier(false)
 	{}
 
-	Image(int xdim, int ydim, cudaStream_t stream, CudaCustomAllocator *allocator):
-		 AccPtr<T>(xdim*ydim, stream, allocator),
-		 x(xdim), y(ydim), z(1), fourier(false)
-	{}
-
-	Image(int xdim, int ydim, int zdim, cudaStream_t stream, CudaCustomAllocator *allocator):
-		 AccPtr<T>(xdim*ydim*zdim, stream, allocator),
-		 x(xdim), y(ydim), z(zdim), fourier(false)
-	{}
-
 	template <typename T1>
-	Image(MultidimArray<T1> img, CudaCustomAllocator *allocator):
-		AccPtr<T>((size_t)img.nzyxdim, allocator),
-		x(img.xdim), y(img.ydim), z(img.zdim), fourier(false)
-	{}
-	
-	template <typename T1>
-	Image(MultidimArray<T1> img):
-		AccPtr<T>((size_t)img.nzyxdim),
+	Image(MultidimArray<T1> img, AccPtrFactory &f):
+		AccPtr<T>(f.make<T>(img.nzyxdim)),
 		x(img.xdim), y(img.ydim), z(img.zdim), fourier(false)
 	{}
 
-	template <typename T1>
-	Image(MultidimArray<T1> img, cudaStream_t stream, CudaCustomAllocator *allocator):
-		 AccPtr<T>((size_t)img.nzyxdim, stream, allocator),
-		x(img.xdim), y(img.ydim), z(img.zdim), fourier(false)
-	{}
-
-	Image(int box_dim, bool is_fourier, bool is3D, CudaCustomAllocator *allocator)
+	Image(int box_dim, bool is_fourier, bool is3D, AccPtrFactory &f)
 	{
 		setSize(box_dim, is_fourier, is3D);
-		AccPtr<T>(x*y*z, allocator);
-	}
-
-	Image(int box_dim, bool is_fourier, bool is3D, cudaStream_t stream, CudaCustomAllocator *allocator)
-	{
-		setSize(box_dim, is_fourier, is3D);
-		AccPtr<T>(x*y*z, stream, allocator);
+		AccPtr<T>(f.make<T>(x*y*z));
 	}
 
 	/*======================================================
@@ -157,6 +124,9 @@ public:
 				setSize(img);
 		}
 
+		if (AccPtr<T>::getHostPtr() == NULL)
+			AccPtr<T>::hostAlloc();
+
 		T *ptr = AccPtr<T>::getHostPtr();
 
 		if (sizeof(T) == sizeof(T1))
@@ -164,6 +134,20 @@ public:
 		else
 			for (int i = 0; i < img.nzyxdim; i++)
 				ptr[i] = (T) img.data[i];
+	}
+
+	template <typename T1>
+	void getHost(MultidimArray<T1> img)
+	{
+		img.resize(AccPtr<T>::getSize());
+
+		T *ptr = AccPtr<T>::getHostPtr();
+
+		if (sizeof(T) == sizeof(T1))
+			memcpy(img.data, ptr, sizeof(T)*img.nzyxdim);
+		else
+			for (int i = 0; i < img.nzyxdim; i++)
+				img.data[i] = (T1) ptr[i];
 	}
 };
 

src/acc/acc_ml_optimiser_impl.h
@@ -277,10 +277,10 @@ void getFourierTransformsAndCtfs(long int my_ori_particle,
 
 		size_t img_size = img.data.nzyxdim;
 
-		AccDataTypes::Image<XFLOAT> temp(img.data, (CudaCustomAllocator *)accMLO->getAllocator());
-		AccDataTypes::Image<XFLOAT> d_img(img.data, (CudaCustomAllocator *)accMLO->getAllocator());
+		AccDataTypes::Image<XFLOAT> temp(img.data, ptrFactory);
+		AccDataTypes::Image<XFLOAT> d_img(img.data, ptrFactory);
 
-		d_img.deviceAlloc();
+		d_img.allAlloc();
 		d_img.hostInit(0);
 		d_img.deviceInit(0);
 
@@ -469,8 +469,7 @@ void getFourierTransformsAndCtfs(long int my_ori_particle,
 			CTIC(accMLO->timer,"softMaskOutsideMap");
 			d_img.cpToHost();
 			d_img.streamSync();
-			for (int i=0; i<img_size; i++)
-				img.data.data[i] = d_img[i];
+			d_img.getHost(img());
 
 			if (is_helical_segment)
 			{
@@ -490,14 +489,12 @@ void getFourierTransformsAndCtfs(long int my_ori_particle,
 		{
 			d_img.cpToHost();
 			d_img.streamSync();
-			for (int i=0; i<img_size; i++)
-				img.data.data[i] = d_img[i];
+			d_img.getHost(img());
 
 			softMaskOutsideMapForHelix(img(), psi_deg, tilt_deg, (baseMLO->particle_diameter / (2. * baseMLO->mymodel.pixel_size)),
 					(baseMLO->helical_tube_outer_diameter / (2. * baseMLO->mymodel.pixel_size)), baseMLO->width_mask_edge);
 
-			for (int i=0; i<img_size; i++)
-				d_img[i] = img.data.data[i];
+			d_img.setHost(img());
 			d_img.cpToDevice();
 		}
 		else
@@ -2757,9 +2754,9 @@ void storeWeightedSums(OptimisationParamters &op, SamplingParameters &sp,
 		RFLOAT dLL;
 
 		if ((baseMLO->iter==1 && baseMLO->do_firstiter_cc) || baseMLO->do_always_cc)
-			dLL = op.min_diff2[ipart];
+			dLL = -op.min_diff2[ipart];
 		else
-			dLL = log(op.sum_weight[ipart]) + op.min_diff2[ipart] - logsigma2;
+			dLL = log(op.sum_weight[ipart]) - op.min_diff2[ipart] - logsigma2;
 
 		// Store dLL of each image in the output array, and keep track of total sum
 		DIRECT_A2D_ELEM(baseMLO->exp_metadata, op.metadata_offset + ipart, METADATA_DLL) = dLL;

commit 3f798da6e998fff5b7859da59f5c46524d17f5d1

get/setHost methods passes multidimarrays by ref
These methods are currently only used by the helical code where
GPU-side functions are missing and we copy down to host-side
temporarily.

With this commit, the helical tag-case test works ok. It
became broken in commit 14e38cc.

src/acc/data_types.h
@@ -125,7 +125,7 @@ public:
 	}
 
 	template <typename T1>
-	void setHost(MultidimArray<T1> img)
+	void setHost(MultidimArray<T1> &img)
 	{
 		if (img.xdim != x || img.ydim != y || img.zdim != z)
 		{
@@ -152,7 +152,7 @@ public:
 	}
 
 	template <typename T1>
-	void getHost(MultidimArray<T1> img)
+	void getHost(MultidimArray<T1> &img)
 	{
 
 		if(img.nzyxdim!=AccPtr<T>::getSize())