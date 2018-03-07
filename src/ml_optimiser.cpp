0d338ac0eb77d690e4c15191b45b40463047c4d1
Sjors Scheres <scheres@mrc-lmb.cam.ac.uk>
2018-03-07 09:38:10 +0000

    some more shifts 3 to data_dim changes

--- src/ml_optimiser.cpp
+++ src/ml_optimiser.cpp
@@ -4657,7 +4657,7 @@ void MlOptimiser::getFourierTransformsAndCtfs(long int my_ori_particle, int ibod
 		MultidimArray<Complex > Fimg, Faux;
 		MultidimArray<RFLOAT> Fctf;
 		Matrix2D<RFLOAT> Aori;
-		Matrix1D<RFLOAT> my_projected_com(3), my_refined_ibody_offset(3);
+		Matrix1D<RFLOAT> my_projected_com(mymodel.data_dim), my_refined_ibody_offset(mymodel.data_dim);
 
 		// Get the right line in the exp_fn_img strings (also exp_fn_recimg and exp_fn_ctfs)
 		int istop = 0;
@@ -5388,7 +5388,7 @@ void MlOptimiser::getFourierTransformsAndCtfs(long int my_ori_particle, int ibod
 
 					// 17May2017: Body is centered at its own COM
 					// move it back to its place in the original particle image
-					Matrix1D<RFLOAT> other_projected_com(3);
+					Matrix1D<RFLOAT> other_projected_com(mymodel.data_dim);
 
 					// Projected COM for this body (using Aori, just like above for ibody and my_projected_com!!!)
 					other_projected_com = Aori * (mymodel.com_bodies[obody]);
@@ -7601,14 +7601,13 @@ void MlOptimiser::storeWeightedSums(long int my_ori_particle, int ibody, int exp
 											DIRECT_A2D_ELEM(exp_metadata, metadata_offset + ipart, icol_tilt) = tilt;
 											RFLOAT old_psi = DIRECT_A2D_ELEM(exp_metadata, metadata_offset + ipart, icol_psi);
 											DIRECT_A2D_ELEM(exp_metadata, metadata_offset + ipart, icol_psi) = psi;
-											Matrix1D<RFLOAT> shifts(2);
+											Matrix1D<RFLOAT> shifts(mymodel.data_dim);
 
 											// include old_offsets for normal refinement (i.e. non multi-body)
 											XX(shifts) = XX(exp_old_offset[ipart]) + oversampled_translations_x[iover_trans];
 											YY(shifts) = YY(exp_old_offset[ipart]) + oversampled_translations_y[iover_trans];
 											if (mymodel.data_dim == 3)
 											{
-												shifts.resize(3);
 												ZZ(shifts) = ZZ(exp_old_offset[ipart]) + oversampled_translations_z[iover_trans];
 											}
 #ifdef DEBUG_BODIES2
