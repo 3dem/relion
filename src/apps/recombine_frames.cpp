1e860e0a4be700db032ba93263e9e3869dc6c756
Jasenko Zivanov <jzivanov@mrc-lmb.cam.ac.uk>
2018-03-22 15:50:10 +0000

    Bfactors are written out as .star files by reconstruct_frames and can be loaded from there (instead of computing them from FCCs); reconstruct_frames recreates the directory structure given by particle .star files, like motion fit

--- src/apps/recombine_frames.cpp
+++ src/apps/recombine_frames.cpp
@@ -50,13 +50,17 @@ class FrameRecomb : public RefinementProgram
 
         FrameRecomb();
 
+            bool hasBfacs, bfac_debug;
             int k0, k1;
             double k0a, k1a;
-            std::string trackFn;
+            std::string trackFn, bfacFn;
 
         int readMoreOptions(IOParser& parser, int argc, char *argv[]);
         int _init();
         int _run();
+
+        std::vector<Image<RFLOAT>> weightsFromFCC();
+        std::vector<Image<RFLOAT>> weightsFromBfacs();
 };
 
 int main(int argc, char *argv[])
@@ -81,8 +85,27 @@ int FrameRecomb::readMoreOptions(IOParser& parser, int argc, char *argv[])
 {
     trackFn = parser.getOption("--t", "Directory with input tracks and FCCs");
 
-    k0a = textToFloat(parser.getOption("--k0", "Min. frequency used in B-factor fit (Angst)", "20"));
-    k1a = textToFloat(parser.getOption("--k1", "Max. frequency used in B-factor fit (Angst)", "-1"));
+    k0a = textToFloat(parser.getOption("--k0", "Min. frequency used in B-factor fit [Angst]", "20"));
+    k1a = textToFloat(parser.getOption("--k1", "Max. frequency used in B-factor fit [Angst]", "-1"));
+
+    s = textToFloat(parser.getOption("--s", "Image size [Px]", "-1"));
+
+    bfacFn = parser.getOption("--b", "A .star file with external B/k-factors", "");
+
+    bfac_debug = parser.checkOption("--bd", "Write out B/k-factor diagnostic data");
+
+    hasBfacs = bfacFn != "";
+
+    if (hasBfacs)
+    {
+        if (s < 0)
+        {
+            std::cerr << "The image size (--s) has to be provided if external B-factors (--b) are used.\n";
+            return 1;
+        }
+
+        sh = s/2 + 1;
+    }
 
     return 0;
 }
@@ -93,6 +116,120 @@ int FrameRecomb::_init()
 }
 
 int FrameRecomb::_run()
+{
+    std::vector<Image<RFLOAT>> freqWeights;
+
+    if (!hasBfacs)
+    {
+        freqWeights = weightsFromFCC();
+    }
+    else
+    {
+        freqWeights = weightsFromBfacs();
+    }
+
+
+    loadInitialMovieValues();
+
+    std::cout << "mg range: " << g0 << ".." << gc << "\n";
+
+    std::vector<ParFourierTransformer> fts(nr_omp_threads);
+
+    double t0 = omp_get_wtime();
+
+    for (long g = g0; g <= gc; g++)
+    {
+        std::cout << "micrograph " << g << " / " << mdts.size() <<"\n";
+
+        std::stringstream stsg;
+        stsg << g;
+
+        const int pc = mdts[g].numberOfObjects();
+
+        std::vector<std::vector<Image<Complex>>> movie;
+
+        try
+        {
+            movie = loadMovie(g, pc, fts);
+        }
+        catch (RelionError XE)
+        {
+            std::cerr << "warning: unable to load micrograph #" << (g+1) << "\n";
+            continue;
+        }
+
+        const int sh = movie[0][0]().xdim;
+        const int s = movie[0][0]().ydim;
+
+        std::string tag = getMicrographTag(g);
+        std::vector<std::vector<d2Vector>> shift;
+
+        std::string tfn = trackFn + "/" + tag + "_tracks.star";
+
+        try
+        {
+            shift = MotionRefinement::readTracks(tfn);
+        }
+        catch (RelionError XE)
+        {
+            std::cerr << "Warning: error reading tracks in " << tfn << "\n";
+            continue;
+        }
+
+        std::string imgTag = getMicrographTag(g);
+        std::string imgName = outPath + "/" + imgTag + ".mrcs";
+
+        mktree(imgName.substr(0, imgName.find_last_of('/')));
+
+        Image<RFLOAT> stack(s,s,1,pc);
+
+        #pragma omp parallel for num_threads(nr_omp_threads)
+        for (int p = 0; p < pc; p++)
+        {
+            int threadnum = omp_get_thread_num();
+
+            Image<Complex> sum(sh,s);
+            sum.data.initZeros();
+
+            Image<Complex> obs(sh,s);
+
+            for (int f = 0; f < fc; f++)
+            {
+                shiftImageInFourierTransform(movie[p][f](), obs(), s, -shift[p][f].x, -shift[p][f].y);
+
+                for (int y = 0; y < s; y++)
+                for (int x = 0; x < sh; x++)
+                {
+                    sum(y,x) += freqWeights[f](y,x) * obs(y,x);
+                }
+            }
+
+            Image<RFLOAT> real(s,s);
+
+            fts[threadnum].inverseFourierTransform(sum(), real());
+
+            for (int y = 0; y < s; y++)
+            for (int x = 0; x < s; x++)
+            {
+                DIRECT_NZYX_ELEM(stack(), p, 0, y, x) = real(y,x);
+            }
+        }
+
+        stack.write(imgName);
+
+        if (debug)
+        {
+            VtkHelper::writeTomoVTK(stack, imgName+".vtk");
+        }
+
+    } // micrographs
+
+    double t1 = omp_get_wtime();
+    double diff = t1 - t0;
+    std::cout << "elapsed (total): " << diff << " sec\n";
+}
+
+std::vector<Image<RFLOAT>> FrameRecomb::weightsFromFCC()
 {
     if (debug) std::cout << "summing up FCCs...\n";
 
@@ -163,7 +300,6 @@ int FrameRecomb::_run()
 
     if (debug) std::cout << "done\n";
 
-
     k0 = (int) angstToPixFreq(k0a);
     k1 = k1a > 0.0? (int) angstToPixFreq(k1a) : sh;
 
@@ -175,25 +311,27 @@ int FrameRecomb::_run()
     std::vector<Image<RFLOAT>> freqWeights;
     freqWeights = DamageHelper::computeWeights(bkFacs.first, sh);
 
-    if (debug)
+    const double cf = 8.0 * angpix*angpix * sh*sh;
+
+    if (bfac_debug)
     {
+        mktree(outPath + "/bfacs");
+
         Image<RFLOAT> bfacFit = DamageHelper::renderBkFit(bkFacs, sh, fc);
         Image<RFLOAT> bfacFitNoScale = DamageHelper::renderBkFit(bkFacs, sh, fc, true);
 
-        ImageLog::write(bfacFit, "bfacs/glob_Bk-fit");
-        ImageLog::write(bfacFitNoScale, "bfacs/glob_Bk-fit_noScale");
-        ImageLog::write(fcc, "bfacs/glob_Bk-data");
-        ImageLog::write(freqWeights, "bfacs/freqWeights");
+        ImageLog::write(bfacFit, outPath + "/bfacs/glob_Bk-fit");
+        ImageLog::write(bfacFitNoScale, outPath + "/bfacs/glob_Bk-fit_noScale");
+        ImageLog::write(fcc, outPath + "/bfacs/glob_Bk-data");
+        ImageLog::write(freqWeights, outPath + "/bfacs/freqWeights");
 
-        std::ofstream bfacsDat("bfacs/Bfac.dat");
-        std::ofstream kfacsDat("bfacs/kfac.dat");
-
-        const double cf = -8.0 * angpix*angpix * sh*sh;
+        std::ofstream bfacsDat(outPath + "/bfacs/Bfac.dat");
+        std::ofstream kfacsDat(outPath + "/bfacs/kfac.dat");
 
         for (int i = 0; i < fc; i++)
         {
             double s = bkFacs.first[i].x;
-            double b = cf/(s*s);
+            double b = -cf/(s*s);
 
             bfacsDat << i << " " << b << "\n";
             kfacsDat << i << " " << log(bkFacs.first[i].y) << "\n";
@@ -203,101 +341,81 @@ int FrameRecomb::_run()
         kfacsDat.close();
     }
 
-    loadInitialMovieValues();
+    MetaDataTable mdt;
+    mdt.setName("perframe_bfactors");
+    mdt.setComment("Written by relion_recombine_frames for --t "+trackFn+".");
 
-    std::cout << "mg range: " << g0 << ".." << gc << "\n";
-
-    std::vector<ParFourierTransformer> fts(nr_omp_threads);
-
-    double t0 = omp_get_wtime();
-
-    for (long g = g0; g <= gc; g++)
+    for (int f = 0; f < fc; f++ )
     {
-        std::cout << "micrograph " << g << " / " << mdts.size() <<"\n";
+        double s = bkFacs.first[f].x;
+        double b = -cf/(s*s);
+        double k = log(bkFacs.first[f].y);
 
-        std::stringstream stsg;
-        stsg << g;
+        mdt.addObject();
+        mdt.setValue(EMDL_IMAGE_FRAME_NR, f);
+        mdt.setValue(EMDL_POSTPROCESS_BFACTOR, b);
+        mdt.setValue(EMDL_POSTPROCESS_GUINIER_FIT_INTERCEPT, k);
+    }
 
-        const int pc = mdts[g].numberOfObjects();
+    mdt.write(outPath + "/bfactors.star");
 
-        std::vector<std::vector<Image<Complex>>> movie;
+    return freqWeights;
+}
 
-        try
+std::vector<Image<double>> FrameRecomb::weightsFromBfacs()
+{
+    MetaDataTable mdt;
+    mdt.read(bfacFn);
+
+    fc = mdt.numberOfObjects();
+
+    std::vector<d2Vector> bkFacs(fc);
+
+    const double cf = 8.0 * angpix*angpix * sh*sh;
+
+    for (int f = 0; f < fc; f++)
+    {
+        int ff;
+        mdt.getValue(EMDL_IMAGE_FRAME_NR, ff);
+
+        double b, k;
+        mdt.getValue(EMDL_POSTPROCESS_BFACTOR, b, f);
+        mdt.getValue(EMDL_POSTPROCESS_GUINIER_FIT_INTERCEPT, k, f);
+
+        bkFacs[f] = d2Vector(sqrt(-cf/b), exp(k));
+    }
+
+    std::vector<Image<RFLOAT>> freqWeights;
+    freqWeights = DamageHelper::computeWeights(bkFacs, sh);
+
+    if (bfac_debug)
+    {
+        mktree(outPath + "/bfacs");
+
+        std::pair<std::vector<d2Vector>,std::vector<double>> bkFacs2;
+        bkFacs2.first = bkFacs;
+        bkFacs2.second = std::vector<double>(fc, 1.0);
+
+        Image<RFLOAT> bfacFitNoScale = DamageHelper::renderBkFit(bkFacs2, sh, fc, true);
+
+        ImageLog::write(bfacFitNoScale, outPath + "/bfacs/glob_Bk-fit_noScale");
+        ImageLog::write(freqWeights, outPath + "/bfacs/freqWeights");
+
+        std::ofstream bfacsDat(outPath + "/bfacs/Bfac.dat");
+        std::ofstream kfacsDat(outPath + "/bfacs/kfac.dat");
+
+        for (int i = 0; i < fc; i++)
         {
-            movie = loadMovie(g, pc, fts);
-        }
-        catch (RelionError XE)
-        {
-            std::cerr << "warning: unable to load micrograph #" << (g+1) << "\n";
-            continue;
-        }
+            double s = bkFacs[i].x;
+            double b = -cf/(s*s);
 
-        const int sh = movie[0][0]().xdim;
-        const int s = movie[0][0]().ydim;
-
-        std::string tag = getMicrographTag(g);
-        std::vector<std::vector<d2Vector>> shift;
-
-        std::string tfn = trackFn + "/" + tag + "_tracks.star";
-
-        try
-        {
-            shift = MotionRefinement::readTracks(tfn);
-        }
-        catch (RelionError XE)
-        {
-            std::cerr << "Warning: error reading tracks in " << tfn << "\n";
-            continue;
-        }
-
-        std::string imgName;
-        mdts[g].getValue(EMDL_IMAGE_NAME, imgName, 0);
-        imgName = outPath + "/" + imgName.substr(imgName.find_last_of("/")+1);
-
-        Image<RFLOAT> stack(s,s,1,pc);
-
-        #pragma omp parallel for num_threads(nr_omp_threads)
-        for (int p = 0; p < pc; p++)
-        {
-            int threadnum = omp_get_thread_num();
-
-            Image<Complex> sum(sh,s);
-            sum.data.initZeros();
-
-            Image<Complex> obs(sh,s);
-
-            for (int f = 0; f < fc; f++)
-            {
-                shiftImageInFourierTransform(movie[p][f](), obs(), s, -shift[p][f].x, -shift[p][f].y);
-
-                for (int y = 0; y < s; y++)
-                for (int x = 0; x < sh; x++)
-                {
-                    sum(y,x) += freqWeights[f](y,x) * obs(y,x);
-                }
-            }
-
-            Image<RFLOAT> real(s,s);
-
-            fts[threadnum].inverseFourierTransform(sum(), real());
-
-            for (int y = 0; y < s; y++)
-            for (int x = 0; x < s; x++)
-            {
-                DIRECT_NZYX_ELEM(stack(), p, 0, y, x) = real(y,x);
-            }
+            bfacsDat << i << " " << b << "\n";
+            kfacsDat << i << " " << log(bkFacs[i].y) << "\n";
         }
 
-        stack.write(imgName);
+        bfacsDat.close();
+        kfacsDat.close();
+    }
 
-        if (debug)
-        {
-            VtkHelper::writeTomoVTK(stack, imgName+".vtk");
-        }
-
-    } // micrographs
-
-    double t1 = omp_get_wtime();
-    double diff = t1 - t0;
-    std::cout << "elapsed (total): " << diff << " sec\n";
+    return freqWeights;
 }
