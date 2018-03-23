aaee5e74ecc478384a8722aa70e800983aa15c51
Jasenko Zivanov <jzivanov@mrc-lmb.cam.ac.uk>
2018-03-23 02:24:36 +0000

    simulated cherry-picking of 16bf5f8

--- src/micrograph_model.cpp
+++ src/micrograph_model.cpp
@@ -338,7 +338,7 @@ void Micrograph::read(FileName fn_in)
 
 	int model_version;
 	model = NULL;
-	if (!MDglobal.getValue(EMDL_MICROGRAPH_MOTION_MODEL_VERSION, model_version)) {
+    if (MDglobal.getValue(EMDL_MICROGRAPH_MOTION_MODEL_VERSION, model_version)) {
 		if (model_version == MOTION_MODEL_THIRD_ORDER_POLYNOMIAL) {
 			model = new ThirdOrderPolynomialModel();
 		} else if (model_version == MOTION_MODEL_NULL) {
@@ -347,7 +347,7 @@ void Micrograph::read(FileName fn_in)
 			std::cerr << "Warning: Ignoring unknown motion model " << model_version << std::endl;
 		}
 	} else {
-//		std::cerr << "Warning: local motion model is absent in the micrograph star file." << std::endl;
+        std::cerr << "Warning: local motion model is absent in the micrograph star file." << std::endl;
 	}
 
 	if (model != NULL) {
