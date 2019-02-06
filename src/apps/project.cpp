af0f387f44fd222ba02ad2d8cd5ececfb757f1cc
Sjors Scheres <scheres@mrc-lmb.cam.ac.uk>
2019-02-06 17:35:17 +0000

    check for half-FFTW CTFs

--- src/apps/project.cpp
+++ src/apps/project.cpp
@@ -511,12 +511,25 @@ public:
 							Ictf.read(fn_ctf);
 							Ictf().setXmippOrigin();
 
-							// Set the CTF-image in Fctf
-							Fctf.resize(F2D);
-							FOR_ALL_ELEMENTS_IN_FFTW_TRANSFORM(Fctf)
+							// If there is a redundant half, get rid of it
+							if (XSIZE(Ictf()) == YSIZE(Ictf()))
 							{
-								// Use negative kp,ip and jp indices, because the origin in the ctf_img lies half a pixel to the right of the actual center....
-								DIRECT_A3D_ELEM(Fctf, k, i, j) = A3D_ELEM(Ictf(), -kp, -ip, -jp);
+								Ictf().setXmippOrigin();
+								FOR_ALL_ELEMENTS_IN_FFTW_TRANSFORM(Fctf)
+								{
+									// Use negative kp,ip and jp indices, because the origin in the ctf_img lies half a pixel to the right of the actual center....
+									DIRECT_A3D_ELEM(Fctf, k, i, j) = A3D_ELEM(Ictf(), -kp, -ip, -jp);
+								}
+							}
+							// otherwise, just window the CTF to the current resolution
+							else if (XSIZE(Ictf()) == YSIZE(Ictf()) / 2 + 1)
+							{
+								windowFourierTransform(Ictf(), Fctf, YSIZE(Fctf));
+							}
+							// if dimensions are neither cubical nor FFTW, stop
+							else
+							{
+								REPORT_ERROR("3D CTF volume must be either cubical or adhere to FFTW format!");
 							}
 						}
 						else
