5097d0aebc345fb45793a15cf10e43c7687a4946
Takanori Nakane <nakane.t@gmail.com>
2019-09-18 12:25:12 +0100

    Prevent divide by zero in skip gridding.

--- src/backprojector.cpp
+++ src/backprojector.cpp
@@ -1450,18 +1450,26 @@ void BackProjector::reconstruct(MultidimArray<RFLOAT> &vol_out,
 			}
 		}
 
+		bool have_warned = false;
 		// perform XMIPP_MAX on all weight elements, and do division of data/weight
 		FOR_ALL_ELEMENTS_IN_FFTW_TRANSFORM(Fweight)
 		{
 			const int r2 = kp * kp + ip * ip + jp * jp;
 			const int ires = FLOOR(sqrt((RFLOAT)r2) / padding_factor);
-			if (ires < r_max)
+			const RFLOAT weight =  XMIPP_MAX(DIRECT_A3D_ELEM(Fweight, k, i, j), DIRECT_A1D_ELEM(radavg_weight, (ires < r_max) ? ires : (r_max - 1)));
+			if (weight == 0)
 			{
-				DIRECT_A3D_ELEM(Fconv, k, i, j) /= XMIPP_MAX(DIRECT_A3D_ELEM(Fweight, k, i, j), DIRECT_A1D_ELEM(radavg_weight, ires));
+				if (!have_warned)
+				{
+					std::cerr << " WARNING: ignoring divide by zero in skip_gridding: ires = " << ires << " kp = " << kp << " ip = " << ip << " jp = " << jp << std::endl;
+					std::cerr << " max_r2 = " << max_r2 << " r_max = " << r_max << " padding_factor = " << padding_factor 
+					           << " ROUND(sqrt(max_r2)) = " << ROUND(sqrt(max_r2)) << " ROUND(r_max * padding_factor) = " << ROUND(r_max * padding_factor) << std::endl;
+					have_warned = true;
+				}
 			}
 			else
 			{
-				DIRECT_A3D_ELEM(Fconv, k, i, j) /= XMIPP_MAX(DIRECT_A3D_ELEM(Fweight, k, i, j), DIRECT_A1D_ELEM(radavg_weight, r_max - 1));
+				DIRECT_A3D_ELEM(Fconv, k, i, j) /= weight;
 			}
 		}
 	}
