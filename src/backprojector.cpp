bd3c349ebb1699337505f422065997bd9146f053
Dari Kimanius <dkimanius@gmail.com>
2019-12-18 17:52:46 +0000

    Fixed scaling issue in ADAM implementation

--- src/backprojector.cpp
+++ src/backprojector.cpp
@@ -1787,6 +1787,9 @@ void BackProjector::reweightGrad(
 	if (lambda1 > 0.)
 		PPmom1.computeFourierTransformMap(mom1, dummy, r_max*2, 1, false);
 
+    RFLOAT mom2_sum(0);
+    int counter(0);
+
 	FOR_ALL_ELEMENTS_IN_FFTW_TRANSFORM(data) // This will also work for 2D
 	{
 		int r2 = kp*kp + ip*ip + jp*jp;
@@ -1801,7 +1804,7 @@ void BackProjector::reweightGrad(
 				}
 				if (lambda2 > 0.)
 				{
-					A3D_ELEM(mom2, kp, ip, jp) = 1;
+					A3D_ELEM(mom2, kp, ip, jp) = 1e-12; //Some small value
 				}
 			}
 
@@ -1823,17 +1826,29 @@ void BackProjector::reweightGrad(
 				A3D_ELEM(PPmom1.data, kp, ip, jp) = v;
 			}
 
+			A3D_ELEM(data, kp, ip, jp) = v;
+
 			if (lambda2 > 0.)
 			{
 				A3D_ELEM(mom2, kp, ip, jp) = lambda2 * A3D_ELEM(mom2, kp, ip, jp) +
 						(1-lambda2) * sqrt(g.real*g.real + g.imag*g.imag);
 
-				if (A3D_ELEM(mom2, kp, ip, jp) > 0.)
-					v /= A3D_ELEM(mom2, kp, ip, jp);
+				if (A3D_ELEM(mom2, kp, ip, jp) > 0)
+				{
+					A3D_ELEM(data, kp, ip, jp) /= A3D_ELEM(mom2, kp, ip, jp);
+					mom2_sum += 1/A3D_ELEM(mom2, kp, ip, jp);
+					counter += 1;
+				}
 			}
+		}
+	}
 
-			A3D_ELEM(data, kp, ip, jp) = v;
-
+	if (lambda2 > 0.)
+	{
+		mom2_sum /= counter;
+		FOR_ALL_DIRECT_ELEMENTS_IN_MULTIDIMARRAY(data)
+		{
+			DIRECT_MULTIDIM_ELEM(data, n) /= mom2_sum;
 		}
 	}
 
@@ -1980,7 +1995,7 @@ void BackProjector::reconstructGrad(
 #endif
 
 			RFLOAT fsc = DIRECT_A1D_ELEM(fsc_spectrum, ires);
-			Complex Fgrad = fsc * A3D_ELEM(data, k, i, j) - (1. - fsc) * A3D_ELEM(PPref.data, k, i, j);
+			Complex Fgrad = fsc * A3D_ELEM(data, k, i, j) - (1. - fsc) / tau2_fudge * A3D_ELEM(PPref.data, k, i, j);
 			A3D_ELEM(PPref.data, k, i, j) += grad_stepsize * Fgrad;
 		}
 	}
