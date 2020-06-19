3dem/relion

commit 35b22622414b2a8837121818cf3c127f0e27e12e

MetadataTable: repaired the label index when skipping rlnSortedIndex (Reported by Kailu Yang on CCPEM)

src/metadata_table.cpp
@@ -1150,18 +1150,18 @@ void MetaDataTable::write(std::ostream& out)
 	{
 		// Write loop header structure
 		out << "loop_ \n";
-		for (long i = 0; i < activeLabels.size(); i++)
+
+		for (long i = 0, n_printed = 0; i < activeLabels.size(); i++)
 		{
 			EMDLabel l = activeLabels[i];
-
 			if (l == EMDL_UNKNOWN_LABEL)
 			{
 				const long offset = unknownLabelPosition2Offset[i];
-				out << "_" << unknownLabelNames[offset]<< " #" << (i + 1) << " \n";
+				out << "_" << unknownLabelNames[offset]<< " #" << (n_printed++) << " \n";
 			}
 			else if (l != EMDL_COMMENT && l != EMDL_SORTED_IDX) // EMDL_SORTED_IDX is only for internal use, never write it out!
 			{
-				out << "_" << EMDL::label2Str(l) << " #" << (i + 1) << " \n";
+				out << "_" << EMDL::label2Str(l) << " #" << (n_printed++) << " \n";
 			}
 		}
 

commit d366c3600a12d3327e3812de3a006c5ffdac0811

MetadataTable: Column numbers should count from 1 (a bug introduced in 35b2262)

src/metadata_table.cpp
@@ -1151,7 +1151,7 @@ void MetaDataTable::write(std::ostream& out)
 		// Write loop header structure
 		out << "loop_ \n";
 
-		for (long i = 0, n_printed = 0; i < activeLabels.size(); i++)
+		for (long i = 0, n_printed = 1; i < activeLabels.size(); i++)
 		{
 			EMDLabel l = activeLabels[i];
 			if (l == EMDL_UNKNOWN_LABEL)