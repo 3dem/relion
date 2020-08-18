daec821c0cad759201c5a5a699f5c2a6b9f3b324
Jasenko Zivanov <jzivanov@mrc-lmb.cam.ac.uk>
2020-08-18 16:39:33 +0200

    cuda test added

---
+++ src/jaz/scripts/cuda_test.cpp
@@ -0,0 +1,21 @@
+
+#ifdef CUDA
+#include <src/jaz/cuda/test00.h>
+#endif
+
+
+int main(int argc, char *argv[])
+{
+	#ifdef CUDA
+
+		CudaTest00 test;
+		test.run();
+
+	#else
+
+		std::cerr << "Not compiled with CUDA." << std::endl;
+
+	#endif
+
+	return 0;
+}
