cc43d0b992416566530fe5f1db120e3569505686
scheres <scheres@mrc-lmb.cam.ac.uk>
2020-10-15 16:31:39 +0100

    remove relative threshold for class_ranker from GUI and disable autoselect options on GUI when TORCH support is off

--- src/gui_jobwindow.cpp
+++ src/gui_jobwindow.cpp
@@ -971,10 +971,30 @@ void JobWindow::initialiseSelectWindow()
 
 	group6 = new Fl_Group(WCOL0,  MENUHEIGHT, 550, 600-MENUHEIGHT, "");
 	group6->end();
+
+#ifdef _TORCH_ENABLED
+
 	place("do_class_ranker", TOGGLE_DEACTIVATE, group6);
 	group6->begin();
 	place("rank_threshold", TOGGLE_DEACTIVATE);
-	place("do_rank_relative", TOGGLE_DEACTIVATE);
+
+#else
+
+	Fl_Text_Buffer *textbuff1 = new Fl_Text_Buffer();
+        textbuff1->text("For auto-selection build with pyTorch, using -DTORCH=ON in cmake.");
+        Fl_Text_Display* textdisp1 = new Fl_Text_Display(XCOL1, current_y, WCOL1+WCOL2+WCOL3+10, STEPY*1.2);
+        textdisp1->textsize(12);
+        textdisp1->color(GUI_BACKGROUND_COLOR);
+        textdisp1->buffer(textbuff1);
+
+        current_y += STEPY*1.5;
+
+	place("do_class_ranker", TOGGLE_ALWAYS_DEACTIVATE, group6);
+	group6->begin();
+	place("rank_threshold", TOGGLE_ALWAYS_DEACTIVATE);
+
+#endif
+
 	group6->end();
 	guientries["do_class_ranker"].cb_menu_i();
 
