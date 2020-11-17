cb06343c66c191af5531ce0ba80d8ecf26bf10b9
scheres <scheres@mrc-lmb.cam.ac.uk>
2020-11-17 09:01:39 +0000

    removed spurious mic_id call

--- src/ml_optimiser.cpp
+++ src/ml_optimiser.cpp
@@ -9659,7 +9659,6 @@ void MlOptimiser::getMetaAndImageDataSubset(long int first_part_id, long int las
 			// Note beamtilt is no longer part of this: it is now in the optics group
 			if (do_ctf_correction)
 			{
-				long int mic_id = mydata.getMicrographId(part_id, img_id);
 				RFLOAT DeltafU, DeltafV, azimuthal_angle, Bfac, kfac, phase_shift;
 
 				if (!mydata.MDimg.getValue(EMDL_CTF_DEFOCUSU, DeltafU, ori_img_id))
