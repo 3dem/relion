8d63b0539b6979fedcdb46879651c113065a1ab8
Dari Kimanius <dkimanius@gmail.com>
2021-01-05 00:13:23 +0000

    New version of external libtorch with RPATH=$ORIGIN.

--- cmake/BuildTorch.cmake
+++ cmake/BuildTorch.cmake
@@ -13,10 +13,10 @@ set(TORCH_EXTERNAL_PATH "${CMAKE_SOURCE_DIR}/external/libtorch")
 find_package(Torch PATHS ${TORCH_EXTERNAL_PATH})
 option(TORCH_EXTERNAL_VERBOSE "Verbose mode during Torch fetch and configure." OFF)
 if(NOT TORCH_FOUND)
-	set(TORCH_FILE_NAME libtorch-commit-4c7fb8c2-cxx11-abi-glibc2.17-shared.zip)
+	set(TORCH_FILE_NAME libtorch-commit-4c7fb8c2-cxx11-abi-glibc2.17-shared_rpath.zip)
 
 	set(TORCH_URL "ftp://ftp.mrc-lmb.cam.ac.uk/pub/dari/${TORCH_FILE_NAME}")
-	set(TORCH_HASH 4aa641cd867d141e438596a1ba31e13d)
+	set(TORCH_HASH 83e0f73d33a8853e359443bfd9a1f928)
 
         set(TORCH_FILE_PATH ${TORCH_EXTERNAL_PATH}/${TORCH_FILE_NAME})
 	
