95899ef175053793d362f72669932e2709a27c62
Dari Kimanius <dkimanius@gmail.com>
2021-01-18 18:50:10 +0000

    Better control of the torch fetch and build process during config-time

--- cmake/BuildTorch.cmake
+++ cmake/BuildTorch.cmake
@@ -9,19 +9,27 @@
 # Date created: 2020-06-07
 #################################################################################
 
-set(TORCH_EXTERNAL_PATH "${CMAKE_SOURCE_DIR}/external/libtorch")
-find_package(Torch PATHS ${TORCH_EXTERNAL_PATH})
 option(TORCH_EXTERNAL_VERBOSE "Verbose mode during Torch fetch and configure." OFF)
+set(TORCH_EXTERNAL_PATH "${CMAKE_SOURCE_DIR}/external/libtorch")
+set(TORCH_FILE_NAME libtorch-commit-4c7fb8c2-cxx11-abi-glibc2.17-shared_rpath.zip)
+set(TORCH_FILE_PATH ${TORCH_EXTERNAL_PATH}/${TORCH_FILE_NAME})
+set(TORCH_URL "ftp://ftp.mrc-lmb.cam.ac.uk/pub/dari/${TORCH_FILE_NAME}")
+set(TORCH_HASH 83e0f73d33a8853e359443bfd9a1f928)
+set(TORCH_FOUND 0)
+
+if(EXISTS ${TORCH_FILE_PATH})
+	file(MD5 ${TORCH_FILE_PATH} CHECKSUM)
+	if (${CHECKSUM} STREQUAL ${TORCH_HASH})
+		find_package(Torch PATHS ${TORCH_EXTERNAL_PATH})
+	else()
+		message(STATUS "Checksum of local file did not match...")
+	endif()
+endif()
+
 if(NOT TORCH_FOUND)
-	set(TORCH_FILE_NAME libtorch-commit-4c7fb8c2-cxx11-abi-glibc2.17-shared_rpath.zip)
-
-	set(TORCH_URL "ftp://ftp.mrc-lmb.cam.ac.uk/pub/dari/${TORCH_FILE_NAME}")
-	set(TORCH_HASH 83e0f73d33a8853e359443bfd9a1f928)
-
-        set(TORCH_FILE_PATH ${TORCH_EXTERNAL_PATH}/${TORCH_FILE_NAME})
-	
-	message(STATUS "Downloading Torch...")
+	file(REMOVE_RECURSE ${TORCH_EXTERNAL_PATH})
 	file(MAKE_DIRECTORY ${TORCH_EXTERNAL_PATH})
+	message(STATUS "Downloading Torch...")
 	foreach(ATTEMPT RANGE 2 6)
 		if (BUILD_TORCH_VERBOSE)
 			execute_process(
@@ -80,3 +88,4 @@ install(DIRECTORY ${TORCH_LIBRARY_DIRS}/ DESTINATION lib FILES_MATCHING PATTERN 
 
 #message(STATUS "TORCH_INCLUDE_DIRS: ${TORCH_INCLUDE_DIRS}")
 #message(STATUS "TORCH_LIBRARY_DIRS: ${TORCH_LIBRARY_DIRS}")
+
