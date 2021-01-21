d88c459bbaecb8a8a8b77d9e76263b6ffbd00a71
Joaquin Oton <joaquin.oton@gmail.com>
2021-01-21 14:23:38 +0000

    Fixed bug. Observation model also considers EMDL_TOMO_TILT_SERIES_PIXEL_SIZE

--- src/jaz/single_particle/obs_model.cpp
+++ src/jaz/single_particle/obs_model.cpp
@@ -51,7 +51,8 @@ ObservationModel::ObservationModel(const MetaDataTable &_opticsMdt, bool do_die_
 {
 	if (!(opticsMdt.containsLabel(EMDL_IMAGE_PIXEL_SIZE) ||
 	      opticsMdt.containsLabel(EMDL_MICROGRAPH_PIXEL_SIZE) ||
-	      opticsMdt.containsLabel(EMDL_MICROGRAPH_ORIGINAL_PIXEL_SIZE))
+	      opticsMdt.containsLabel(EMDL_MICROGRAPH_ORIGINAL_PIXEL_SIZE) ||
+	      opticsMdt.containsLabel(EMDL_TOMO_TILT_SERIES_PIXEL_SIZE))
 	  || !opticsMdt.containsLabel(EMDL_CTF_VOLTAGE)
 	  || !opticsMdt.containsLabel(EMDL_CTF_CS))
 	{
