eff217956455eb01ce62a31f54c2388912fcf446
scheres <scheres@mrc-lmb.cam.ac.uk>
2021-05-10 16:19:53 +0100

    removed debug comment pseudo gold standard

--- src/ml_optimiser.cpp
+++ src/ml_optimiser.cpp
@@ -2910,8 +2910,8 @@ void MlOptimiser::iterate()
 		else if (do_grad)
 			REPORT_ERROR("ERROR: Random seed must be set for gradient optimisation.");
 
-		if (grad_pseudo_halfsets)
-			std::cerr << "DEBUG: doing pseudo gold standard" << std::endl;
+		//if (grad_pseudo_halfsets)
+		//	std::cerr << "DEBUG: doing pseudo gold standard" << std::endl;
 
 		expectation();
 
