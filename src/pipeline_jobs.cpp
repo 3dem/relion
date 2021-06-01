0416e7cf37d5e2b3803b171966ccac06ed7b2de7
scheres <scheres@mrc-lmb.cam.ac.uk>
2021-06-01 15:22:27 +0100

    now hopefully correct for reextract coordinates

--- src/pipeline_jobs.cpp
+++ src/pipeline_jobs.cpp
@@ -2447,7 +2447,7 @@ bool RelionJob::getCommandsExtractJob(std::string &outputname, std::vector<std::
 	if (joboptions["do_reextract"].getBoolean())
 	{
 		FileName fn_pickstar = outputname + "extractpick.star";
-		Node node(fn_pickstar, LABEL_EXTRACT_PARTS_REEX);
+		Node node(fn_pickstar, LABEL_EXTRACT_COORDS_REEX);
 		outputNodes.push_back(node);
 		command += " --pick_star " + fn_pickstar;
 	}
@@ -2455,7 +2455,7 @@ bool RelionJob::getCommandsExtractJob(std::string &outputname, std::vector<std::
 	if (joboptions["do_extract_helix"].getBoolean() && joboptions["do_extract_helical_tubes"].getBoolean())
 	{
 		FileName fn_pickstar = outputname + "extractpick.star";
-		Node node(fn_pickstar, LABEL_EXTRACT_PARTS_HELIX);
+		Node node(fn_pickstar, LABEL_EXTRACT_COORDS_HELIX);
 		outputNodes.push_back(node);
 		command += " --pick_star " + fn_pickstar;
 	}
@@ -2526,8 +2526,7 @@ bool RelionJob::getCommandsExtractJob(std::string &outputname, std::vector<std::
 				command += " --helical_nr_asu 1 --helical_rise 1";
 		}
 	}
-
-	if (!joboptions["do_reextract"].getBoolean() && !joboptions["do_extract_helix"].getBoolean())
+	else
 	{
 		Node node3(fn_ostar, LABEL_EXTRACT_PARTS);
 		outputNodes.push_back(node3);
