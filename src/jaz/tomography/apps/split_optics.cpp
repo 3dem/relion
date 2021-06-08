33ee17d412a0100d27d5bd86407628028e69a0b8
Jasenko Zivanov <jzivanov@mrc-lmb.cam.ac.uk>
2021-06-09 01:32:29 +0200

    empty split_optics program added

---
+++ src/jaz/tomography/apps/split_optics.cpp
@@ -0,0 +1,17 @@
+#include <src/args.h>
+#include <src/jaz/util/log.h>
+
+
+int main(int argc, char *argv[])
+{
+	try
+	{
+
+	}
+	catch (RelionError e)
+	{
+		return RELION_EXIT_FAILURE;
+	}
+
+	return RELION_EXIT_SUCCESS;
+}
