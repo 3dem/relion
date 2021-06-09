b3f22da2e98b576017e9e154a4fe04a69e667f6b
Jasenko Zivanov <jzivanov@mrc-lmb.cam.ac.uk>
2021-06-09 17:49:13 +0200

    motion_refine can crop images again

--- src/jaz/image/padding.h
+++ src/jaz/image/padding.h
@@ -6,7 +6,7 @@
 
 class Padding
 {
-    public:
+	public:
 		
 		template <class T>
 		static BufferedImage<T> padCorner2D_half(const RawImage<T> &img, int w, int h);
@@ -28,66 +28,69 @@ class Padding
 		
 		template <class T>
 		static BufferedImage<T> unpadCenter3D_full(const RawImage<T> &img, int b);
+		
+		template <class T>
+		static void copyUnpaddedCenter2D_full(const RawImage<T>& src, RawImage<T>& dest);
 };
 
 template <class T>
 BufferedImage<T> Padding::padCorner2D_half(const RawImage<T>& img, int w, int h)
 {
-    const int w0 = img.xdim;
-    const int h0 = img.ydim;
-
-    BufferedImage<T> out(w,h);
-
-    for (int y = 0; y < h; y++)
-    for (int x = 0; x < w; x++)
-    {
-        const int x1 = x;
-        const int y1 = y < h/2? y : y - h;
-
-        if (x1 < w0/2 && y1 < h0/2 && x1 >= -w0/2 && y1 >= -h0/2)
-        {
-            const int x0 = x1;
-            const int y0 = y1 < 0? y1 + h0 : y1;
-
-            out(x,y) = img(x0,y0);
-        }
-        else
-        {
-            out(x,y) = T(0);
-        }
-    }
-
-    return out;
+	const int w0 = img.xdim;
+	const int h0 = img.ydim;
+	
+	BufferedImage<T> out(w,h);
+	
+	for (int y = 0; y < h; y++)
+	for (int x = 0; x < w; x++)
+	{
+		const int x1 = x;
+		const int y1 = y < h/2? y : y - h;
+		
+		if (x1 < w0/2 && y1 < h0/2 && x1 >= -w0/2 && y1 >= -h0/2)
+		{
+			const int x0 = x1;
+			const int y0 = y1 < 0? y1 + h0 : y1;
+			
+			out(x,y) = img(x0,y0);
+		}
+		else
+		{
+			out(x,y) = T(0);
+		}
+	}
+	
+	return out;
 }
 
 template <class T>
 BufferedImage<T> Padding::padCorner2D_full(const RawImage<T>& img, int w, int h)
 {
-    const int w0 = img.xdim;
-    const int h0 = img.ydim;
-
-    BufferedImage<T> out(w,h);
-
-    for (int y = 0; y < h; y++)
-    for (int x = 0; x < w; x++)
-    {
-        const int x1 = x < w/2? x : x - w;
-        const int y1 = y < h/2? y : y - h;
-
-        if (x1 < w0/2 && y1 < h0/2 && x1 >= -w0/2 && y1 >= -h0/2)
-        {
-            const int x0 = x1 < 0? x1 + w0 : x1;
-            const int y0 = y1 < 0? y1 + h0 : y1;
-
-            out(x,y) = img(x0,y0);
-        }
-        else
-        {
-            out(x,y) = T(0);
-        }
-    }
-
-    return out;
+	const int w0 = img.xdim;
+	const int h0 = img.ydim;
+	
+	BufferedImage<T> out(w,h);
+	
+	for (int y = 0; y < h; y++)
+	for (int x = 0; x < w; x++)
+	{
+		const int x1 = x < w/2? x : x - w;
+		const int y1 = y < h/2? y : y - h;
+		
+		if (x1 < w0/2 && y1 < h0/2 && x1 >= -w0/2 && y1 >= -h0/2)
+		{
+			const int x0 = x1 < 0? x1 + w0 : x1;
+			const int y0 = y1 < 0? y1 + h0 : y1;
+			
+			out(x,y) = img(x0,y0);
+		}
+		else
+		{
+			out(x,y) = T(0);
+		}
+	}
+	
+	return out;
 }
 
 template <class T>
@@ -95,17 +98,17 @@ BufferedImage<T> Padding::padCenter2D_full(const RawImage<T> &img, int b)
 {
 	const int w0 = img.xdim;
 	const int h0 = img.ydim;
-
-    BufferedImage<T> out(w0 + 2*b, h0 + 2*b, 1);
+	
+	BufferedImage<T> out(w0 + 2*b, h0 + 2*b, 1);
 	out.fill(0);
 	
 	for (int y = 0; y < h0; y++)
-    for (int x = 0; x < w0; x++)
-    {
-        out(x+b,y+b) = img(x,y);
-    }
-
-    return out;
+	for (int x = 0; x < w0; x++)
+	{
+		out(x+b,y+b) = img(x,y);
+	}
+	
+	return out;
 }
 
 template <class T>
@@ -114,18 +117,18 @@ BufferedImage<T> Padding::padCenter3D_full(const RawImage<T> &img, int b)
 	const int w0 = img.xdim;
 	const int h0 = img.ydim;
 	const int d0 = img.zdim;
-
-    BufferedImage<T> out(w0 + 2*b, h0 + 2*b, d0 + 2*b);
+	
+	BufferedImage<T> out(w0 + 2*b, h0 + 2*b, d0 + 2*b);
 	out.fill(0);
 	
 	for (int z = 0; z < d0; z++)
 	for (int y = 0; y < h0; y++)
-    for (int x = 0; x < w0; x++)
-    {
-        out(x+b,y+b,z+b) = img(x,y,z);
-    }
-
-    return out;
+	for (int x = 0; x < w0; x++)
+	{
+		out(x+b,y+b,z+b) = img(x,y,z);
+	}
+	
+	return out;
 }
 
 template <class T>
@@ -134,17 +137,17 @@ BufferedImage<T> Padding::unpadCenter2D_full(const RawImage<T> &img, int b)
 	const int w0 = img.xdim - 2*b;
 	const int h0 = img.ydim - 2*b;
 	const int fc = img.zdim;
-
-    BufferedImage<T> out(w0,h0,fc);
+	
+	BufferedImage<T> out(w0,h0,fc);
 	
 	for (int f = 0; f < fc; f++)
 	for (int y = 0; y < h0; y++)
-    for (int x = 0; x < w0; x++)
-    {
-        out(x,y,f) = img(x+b, y+b, f);
-    }
+	for (int x = 0; x < w0; x++)
+	{
+		out(x,y,f) = img(x+b, y+b, f);
+	}
 
-    return out;
+	return out;
 }
 
 template <class T>
@@ -153,20 +156,20 @@ BufferedImage<T> Padding::unpadCorner2D_full(const RawImage<T> &img, int b)
 	const int w0 = img.xdim - 2*b;
 	const int h0 = img.ydim - 2*b;
 	const int fc = img.zdim;
-
-    BufferedImage<T> out(w0,h0,fc);
+	
+	BufferedImage<T> out(w0,h0,fc);
 	
 	for (int f = 0; f < fc; f++)
 	for (int y = 0; y < h0; y++)
-    for (int x = 0; x < w0; x++)
-    {
+	for (int x = 0; x < w0; x++)
+	{
 		const int xx = x < w0/2? x : x - w0 + img.xdim;
 		const int yy = y < h0/2? y : y - h0 + img.ydim;
 		
-        out(x,y,f) = img(xx, yy, f);
-    }
-
-    return out;
+		out(x,y,f) = img(xx, yy, f);
+	}
+	
+	return out;
 }
 
 template <class T>
@@ -175,17 +178,38 @@ BufferedImage<T> Padding::unpadCenter3D_full(const RawImage<T> &img, int b)
 	const int w0 = img.xdim - 2*b;
 	const int h0 = img.ydim - 2*b;
 	const int d0 = img.zdim - 2*b;
-
-    BufferedImage<T> out(w0,h0,d0);
+	
+	BufferedImage<T> out(w0,h0,d0);
 	
 	for (int z = 0; z < d0; z++)
 	for (int y = 0; y < h0; y++)
-    for (int x = 0; x < w0; x++)
-    {
-        out(x,y,z) = img(x+b, y+b, z+b);
-    }
+	for (int x = 0; x < w0; x++)
+	{
+		out(x,y,z) = img(x+b, y+b, z+b);
+	}
+	
+	return out;
+}
 
-    return out;
+template<class T>
+void Padding::copyUnpaddedCenter2D_full(
+		const RawImage<T>& src, 
+		RawImage<T>& dest)
+{
+	const int w_src  = src.xdim;
+	const int h_src  = src.ydim;
+	
+	const int w_dest  = dest.xdim;
+	const int h_dest  = dest.ydim;
+	
+	const int bx = (w_src - w_dest) / 2;
+	const int by = (h_src - h_dest) / 2;
+	
+	for (int y = 0; y < h_dest; y++)
+	for (int x = 0; x < w_dest; x++)
+	{
+		dest(x,y) = src(x+bx, y+by);
+	}
 }
 
 #endif
