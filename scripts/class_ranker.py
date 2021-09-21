3c98be244293ecbe968c90775280bc7756390818
dkimanius <dkimanius@gmail.com>
2021-09-21 11:58:03 +0100

    Classranker py-script: Removed 'packaging' module dependency and removed the call to 'to(cpu)' for backward compatipility.

--- scripts/class_ranker.py
+++ scripts/class_ranker.py
@@ -1,22 +1,15 @@
 import os
 import argparse
 import sys
-from packaging import version
 
 try:
     import torch
-    if version.parse(torch.__version__) < version.parse("1.0.1"):
-        print("PYTHON ERROR: Module 'torch' is too old (must be >= 1.0.1).")
-        exit(1)
 except ImportError:
     print("PYTHON ERROR: The required python module 'torch' was not found.")
     exit(1)
 
 try:
     import numpy as np
-    if version.parse(np.__version__) < version.parse("1.0.0"):
-        print("PYTHON ERROR: Module 'numpy' is too old (must be >= 1.0.0).")
-        exit(1)
 except ImportError:
     print("PYTHON ERROR: The required python module 'numpy' was not found.")
     exit(1)
@@ -37,8 +30,6 @@ features = np.load(feature_fn)
 images = np.load(images_fn)
 
 count = features.shape[0]
-
-model = model.to("cpu")
 
 features_tensor = torch.Tensor(features)
 images_tensor = torch.unsqueeze(torch.Tensor(images), 1)
