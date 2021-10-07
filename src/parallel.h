7a535a61ee1e9b0f3e529ef76c59abb921efd9e7
Takanori Nakane <nakane.t@gmail.com>
2021-10-07 05:19:18 +0100

    paralle.h: use OpenMPI mutex for Mutex, get rid of Barrier and ThreadManager.

--- src/parallel.h
+++ src/parallel.h
@@ -69,7 +69,7 @@ typedef void (*ThreadFunction) (ThreadArgument &arg);
 class Mutex
 {
 private:
-    pthread_mutex_t mutex; //our pthread mutex
+    omp_lock_t mutex; //our pthread mutex
 
 public:
     /** Default constructor.
@@ -95,64 +95,6 @@ public:
     virtual void unlock();
 };//end of class Mutex
 
-/** Class to synchronize several threads in some point of execution.
- * Threads is a way of distributing workload across
- * different workers to solve a problem faster. Nevertheless, sometimes
- * we need synchronization between threads to avoid undesired race
- * conditions and other problems. Here we are an implementation of a barrier
- * that allows putting all threads to wait at a given point until all of them
- * have reached such point and can continue working. Barriers are usually
- * available through pthreads system library. Nonetheless, sometimes it is not
- * so we have to implement it here.
- * @code
- * Mutex mutex;
- *
- * //Then in each thread to access the critical section:
- *  mutex.lock();
- *  //...Do critical section stuff
- *  mutex.unlock();
- *
-   @endcode
- */
-class Barrier
-{
-
-private:
-    int needed; ///< How many threads should arrive to meet point
-    int called; ///< How many threads already arrived
-    pthread_mutex_t mutex; ///< Mutex to update structure
-    pthread_cond_t cond; ///< Condition on which the threads are waiting
-
-public:
-    /** Constructor of the barrier to initialize the object.
-     * You should pass the number of working threads that
-     * you want to wait on the barrier. The internal counter
-     * of the barrier will be initialized with numberOfThreads + 1
-     * taking into account the main thread, so it need to wait
-     * also in the barrier with the worker threads to all
-     * can move on.
-     * @code
-     *  //For syncronize 10 threads created by a main thread
-     *  //you can create the barrier from the main thread
-     *  Barrier * barrier = new Barrier(10);
-     *  //...
-     *  //In the syncronization point
-     *  barrier->wait();
-     * @endcode
-     * */
-    Barrier(int numberOfThreads);
-
-    /** Destructor to free all memory used */
-    ~Barrier();
-
-    /** Request to wait in this meet point.
-     * For each thread calling this function the execution will
-     * be paused untill all threads arrive this point.
-     */
-    void wait();
-
-};//end of class Barrier
-
 /** Class to pass arguments to threads functions.
  * The argument passed can be obtained casting
  * the void * data received in the function.
@@ -175,97 +117,6 @@ public:
 };
 
 void * _threadMain(void * data);
-
-/** Class for manage a group of threads performing one or several tasks.
- * This class is very useful when we have some function that can be executed
- * in parrallel by threads. The threads are created in the contructor of the object
- * and released in destructor. This way threads can execute different
- * functions at diffent moments and exit at the end of manager life. Also, the
- * wait() function allow in the main thread to wait until all threads have
- * finish working on a task and maybe then execute another one.
- * This class is supposed to be used only in the main thread.
- */
-class ThreadManager
-{
-private:
-    int threads; ///< number of working threads.
-    pthread_t * ids; ///< pthreads identifiers
-    ThreadArgument * arguments; ///< Arguments passed to threads
-    Barrier * barrier; ///< barrier for syncronized between tasks.
-    /// Pointer to the function to work on,
-    /// if null threads should exit
-    ThreadFunction workFunction;
-    bool started;
-    void * workClass;
-
-    void startThreads();
-
-public:
-    /** Constructor, number of working threads should be supplied */
-    ThreadManager(int numberOfThreads, void * workClass = NULL);
-
-    /** Destructor, free memory and exit threads */
-    ~ThreadManager();
-
-    /** Function to start working in a task.
-     * The function that want to be executed in parallel
-     * by the working threads should be passed as argument.
-     * Functions that can be executed by thread should by of the
-     * type ThreadFunction, i.e., return void * and only
-     * one argument of type ThreadArgument.
-     * The call of this function will block the main thread
-     * until all workers finish their job, if you dont want to block
-     * use runAsync instead, and later can call wait for waiting
-     * until threads are done.
-     * @code
-     *
-     *  //Global variables, so it are visible in 'processSeveralImages()'
-     *  ParallelTaskDistributor * td;
-     *  //function to perform some operation
-     *  //to N images executed in parellel
-     *  void * processImages(ThreadArgument & data)
-     *  {
-     *      int thread_id = arg.thread_id;
-     *
-     *      size_t firstImage, lastImage;
-     *      while (td->getTasks(firstImage, lastImage))
-     *          for (int image = firstImage; image <= lastImage; ++image)
-     *          {
-     *              //...
-     *              processOneImage(image);
-     *              //...
-     *          }
-     *  }
-     *
-     *  int main()
-     *  {
-     *  //...
-     *  //Distribute 1000 tasks in blocks of 100.
-     *  td = new ThreadTaskDistributor(1000, 100);
-     *  //Start 2 threads to work on it
-     *  ThreadManager * tm = new ThreadManager(2);
-     *  tm.run(processImages);
-     *  //...
-     *  //Same threads can work in other function
-     *  tm.run(processVolumes);
-     *  }
-     * @endcode
-     */
-    void run(ThreadFunction function);
-
-    /** Same as run but without blocking. */
-    void runAsync(ThreadFunction function);
-
-    /** Function that should be called to wait until all threads finished work */
-    void wait();
-
-    /** function to start running the threads.
-     * Should be external and declared as friend */
-    friend void * _threadMain(void * data);
-
-
-}
-;//end of class ThreadManager
 
 /** This class distributes dynamically N tasks between parallel workers.
  * @ingroup ParallelLibrary
