61f64a74f5644f0191ade69d2e436acc2e65c910
scheres <scheres@mrc-lmb.cam.ac.uk>
2022-04-06 16:53:04 +0100

    removed Import jobtype from tomo GUI as no longer makes sense with motioncorr and ctffind jobtypes for tomo

--- src/gui_mainwindow.cpp
+++ src/gui_mainwindow.cpp
@@ -408,12 +408,14 @@ GuiMainWindow::GuiMainWindow(int w, int h, const char* title, FileName fn_pipe,
 	if (_do_tomo)
 	{
 
-		browse_grp[nr_browse_tabs] = new Fl_Group(WCOL0, 2, 550, 615-MENUHEIGHT);
+		/* SHWS 6Apr2022: with new motioncorr and ctffind jobtypes, the old Import makes no sesne anymore, so removed for now
+        browse_grp[nr_browse_tabs] = new Fl_Group(WCOL0, 2, 550, 615-MENUHEIGHT);
 		browser->add("Tomo import");
 		gui_jobwindows[nr_browse_tabs] = new JobWindow();
 		gui_jobwindows[nr_browse_tabs]->initialise(PROC_TOMO_IMPORT);
 		browse_grp[nr_browse_tabs]->end();
 		nr_browse_tabs++;
+        */
 
 		browse_grp[nr_browse_tabs] = new Fl_Group(WCOL0, 2, 550, 615-MENUHEIGHT);
 		browser->add("Motion correction");
