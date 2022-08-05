2fee56c6f30526b2d2f3b780b0ae87fd9cb2e371
Suyash Tandon <suyash.tandon@amd.com>
2022-08-05 08:09:37 -0500

    adding a HIP-port in `src/acc/hip/`
     type: src

---
+++ src/acc/hip/hip_backprojector.hip.cpp
@@ -0,0 +1,7 @@
+#include <signal.h>
+#include <hip/hip_runtime.h>
+#include "src/acc/hip/hip_settings.h"
+#include "src/acc/acc_backprojector.h"
+#include "src/acc/acc_projector.h"
+
+#include "src/acc/acc_backprojector_impl.h"
---
+++ src/acc/hip/hip_projector.hip.cpp
@@ -0,0 +1,5 @@
+#include "src/acc/acc_projector.h"
+#include <signal.h>
+
+#include "src/acc/acc_projector_impl.h"
+
