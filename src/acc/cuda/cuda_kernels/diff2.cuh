0f25244d302c2f003cdae9fff9740dd37144cd7e
scheres <scheres@mrc-lmb.cam.ac.uk>
2022-09-23 15:57:30 +0100

    this is probably inefficient and needs discussion with Dari, but it removed irreproducibility from maxCC runs.... I dont understand why

--- src/acc/cuda/cuda_kernels/diff2.cuh
+++ src/acc/cuda/cuda_kernels/diff2.cuh
@@ -453,7 +453,9 @@ __global__ void cuda_kernel_diff2_CC_coarse(
 #ifdef ACC_DOUBLE_PRECISION
 	g_diff2s[iorient * translation_num + itrans] = - ( s_weight[0] / sqrt(s_norm[0]));
 #else
-	g_diff2s[iorient * translation_num + itrans] += - ( s_weight[0] / sqrtf(s_norm[0]));
+    cuda_atomic_add(&g_diff2s[iorient * translation_num + itrans], - ( s_weight[0] / (block_sz * sqrtf(s_norm[0]))) );
+    //g_diff2s[iorient * translation_num + itrans] += - ( s_weight[0] / sqrtf(s_norm[0]));
+
 #endif
 }
 
