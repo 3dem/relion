64b739cec1af26d33eab7f7afe0111bb2973c7a7
scheres <scheres@mrc-lmb.cam.ac.uk>
2022-09-23 16:35:03 +0100

    same correction of maxCC for double precision

--- src/acc/cuda/cuda_kernels/diff2.cuh
+++ src/acc/cuda/cuda_kernels/diff2.cuh
@@ -451,7 +451,8 @@ __global__ void cuda_kernel_diff2_CC_coarse(
 		__syncthreads();
 	}
 #ifdef ACC_DOUBLE_PRECISION
-	g_diff2s[iorient * translation_num + itrans] = - ( s_weight[0] / sqrt(s_norm[0]));
+    cuda_atomic_add(&g_diff2s[iorient * translation_num + itrans], - ( s_weight[0] / (block_sz * sqrt(s_norm[0]))) );
+    g_diff2s[iorient * translation_num + itrans] += - ( s_weight[0] / sqrt(s_norm[0]));
 #else
     cuda_atomic_add(&g_diff2s[iorient * translation_num + itrans], - ( s_weight[0] / (block_sz * sqrtf(s_norm[0]))) );
     //g_diff2s[iorient * translation_num + itrans] += - ( s_weight[0] / sqrtf(s_norm[0]));
