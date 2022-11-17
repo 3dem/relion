ed5780c01ed3bdb4bdb87a2b8b28ca37616ea182
alisterburt <alisterburt@gmail.com>
2022-11-17 10:47:17 +0000

    add fixture to run tests in project directory (#70)

--- src/tomography_python_programs/_tests/test_metadata_model.py
+++ src/tomography_python_programs/_tests/test_metadata_model.py
@@ -7,7 +7,7 @@ def test_read_single_tilt_series_metadata(tilt_series_star_file):
     assert tilt_series.data.shape == (41, 29)
 
 
-def test_read_global_metadata(global_relion_metadata_file):
+def test_read_global_metadata(global_relion_metadata_file, run_in_project_directory):
     """Simple parsing test."""
     relion_metadata = RelionTiltSeriesSet.from_star_file(global_relion_metadata_file)
     assert isinstance(relion_metadata, RelionTiltSeriesSet)
@@ -16,14 +16,14 @@ def test_read_global_metadata(global_relion_metadata_file):
     assert relion_metadata.global_data.shape == (5, 13)
 
 
-def test_global_metadata_correctly_ordered(relion_metadata):
+def test_global_metadata_correctly_ordered(relion_metadata, run_in_project_directory):
     """Ordering of tilt-series should be consistent across metadata"""
     names_a = relion_metadata.global_data['rlnTomoName']
     names_b = [tilt_series.name for tilt_series in relion_metadata.tilt_series]
     assert all([a == b for a, b in zip(names_a, names_b)])
 
 
-def test_gui_data_model_from_relion_metadata(relion_metadata):
+def test_gui_data_model_from_relion_metadata(relion_metadata, run_in_project_directory):
     """Test correct generation of simple data model compatible with GUI components."""
     gui_model = relion_metadata.as_gui_model()
     assert isinstance(gui_model, dict)
@@ -36,7 +36,7 @@ def test_gui_data_model_from_relion_metadata(relion_metadata):
     )
 
 
-def test_gui_data_model_includes_tomogram_file(relion_metadata):
+def test_gui_data_model_includes_tomogram_file(relion_metadata, run_in_project_directory):
     """Test that gui model generated from relion metadata references tomograms."""
     gui_model = relion_metadata.as_gui_model()
     tilt_series = gui_model[list(gui_model.keys())[0]]
