7d4bd5a2bcf8629069081470cd2744d398bd2641
scheres <scheres@mrc-lmb.cam.ac.uk>
2023-10-04 14:45:04 +0100

    repaired picktomo of particles, not passing spacing param

--- src/pipeline_jobs.cpp
+++ src/pipeline_jobs.cpp
@@ -7170,7 +7170,8 @@ bool RelionJob::getCommandsTomoPickTomogramsJob(std::string &outputname, std::ve
     command2 += " --annotations-directory " + outputname + "annotations";
     command2 += " --output-directory " + outputname;
 
-    if (joboptions["pick_mode"].getString() != "particles")
+    FileName fnt = joboptions["pick_mode"].getString();
+    if (!fnt.contains("particles"))
         command2 += " --spacing-angstroms " + joboptions["particle_spacing"].getString();
 
     // Other arguments for get-poses
