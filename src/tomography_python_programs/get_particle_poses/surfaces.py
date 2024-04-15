Fix angle calculation

src/tomography_python_programs/get_particle_poses/surfaces.py
@@ -12,21 +12,25 @@ from .._utils.cgal import Polyhedron
 
 COMMAND_NAME = 'surfaces'
 
-def vec2euler(vec: np.ndarray, random_rot: bool=True) -> np.ndarray:
-    """Vector is [Z, Y, X] of shape (3,) or (N, 3). The vectors have to be normalized.
-    Returns array of shape (N, 3) with [rot, tilt, psi] in radians."""
-    vec = vec.reshape((-1, 3))
-    out = np.empty_like(vec, dtype=float)
-    # psi: rotation around global Z axis
-    np.arctan2(vec[:, 1], -vec[:, 2], out=out[:, 2])
-    # tilt: rotation around new Y axis
-    np.arccos(vec[:, 0], out=out[:, 1])
-    # rot: rotation around new Z axis (random)
-    if random_rot:
-        out[:, 0] = np.random.rand(len(out)) * 2 * np.pi - np.pi
-    else:
-        out[:, 0] = 0
-    return out
+def vector2matrix(z_vectors: np.ndarray):
+    """Turn normal vector into rotation matrix
+    Stolen from the PoseSampler class in MorphoSampler.
+    """
+    z_vectors /= np.linalg.norm(z_vectors, axis=-1, keepdims=True)
+    y_vectors = np.cross(z_vectors, [1, 1, 1])
+    y_vectors /= np.linalg.norm(y_vectors, axis=-1, keepdims=True)
+    x_vectors = np.cross(y_vectors, z_vectors)
+    x_vectors /= np.linalg.norm(x_vectors, axis=-1, keepdims=True)
+    orientations = np.empty(shape=(len(z_vectors), 3, 3), dtype=np.float32)
+    orientations[:, :, 0] = x_vectors
+    orientations[:, :, 1] = y_vectors
+    orientations[:, :, 2] = z_vectors
+
+    # randomise in plane rotation
+    angles = np.random.uniform(0, 360, size=(len(z_vectors)))
+    Rz = R.from_euler('z', angles=angles, degrees=True).as_matrix()
+    return orientations @ Rz
+
 
 
 @cli.command(name=COMMAND_NAME, no_args_is_help=True)
@@ -49,6 +53,7 @@ def derive_poses_on_surfaces(
     global_df = global_df.set_index('rlnTomoName')
     annotation_files = annotations_directory.glob('*_surfaces.npz')
     dfs = []
+    rotated_basis = R.from_euler('y', angles=90, degrees=True).as_matrix()
     for file in annotation_files:
         surface_file = np.load(file)
         tilt_series_id = '_'.join(file.name.split('_')[:-1])
@@ -64,7 +69,10 @@ def derive_poses_on_surfaces(
         vertices = polyhedron.vertices_array()
         normals = polyhedron.compute_vertex_normals()
         assert len(vertices) == len(normals)
-        eulers = np.rad2deg(vec2euler(normals))
+        rotated_orientations = vector2matrix(normals[:, (2, 1, 0)]) @ rotated_basis
+        eulers = R.from_matrix(rotated_orientations).inv().as_euler(
+            seq='ZYZ', degrees=True,
+        )
         assert len(vertices) == len(eulers)
         data = {
             'rlnTomoName': [tilt_series_id] * len(vertices),
@@ -77,7 +85,6 @@ def derive_poses_on_surfaces(
         }
         dfs.append(pd.DataFrame(data))
     df = pd.concat(dfs)
-    rotated_basis = R.from_euler('y', angles=90, degrees=True).as_matrix()
     rot_prior, tilt_prior, psi_prior = R.from_matrix(rotated_basis).as_euler(
         seq='ZYZ', degrees=True
     )