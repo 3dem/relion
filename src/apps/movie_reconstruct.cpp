3dem/relion

commit 9d0df80f3204fe0eb5cc6ab404e372a084657d30

EER: supported 2K detectors in loadEERGain(). This function is no longer static.

src/apps/movie_reconstruct.cpp
@@ -289,16 +289,6 @@ void MovieReconstructor::backproject(int rank, int size)
 				eer_grouping = requested_eer_grouping;
 		}
 
-		FileName fn_gain = mic.getGainFilename();
-		if (fn_gain != prev_gain)
-		{
-			if (isEER)
-				EERRenderer::loadEERGain(fn_gain, Igain(), eer_upsampling);
-			else
-				Igain.read(fn_gain);
-			prev_gain = fn_gain;
-		}
-
 		// Read trajectories. Both particle ID and frame ID are 0-indexed in this array.
 		std::vector<std::vector<gravis::d2Vector>> trajectories = MotionHelper::readTracksInPix(fn_traj, movie_angpix);
 
@@ -321,6 +311,17 @@ void MovieReconstructor::backproject(int rank, int size)
 		}
 		const int w0 = XSIZE(Iframe());
 		const int h0 = YSIZE(Iframe());
+
+		// Read the gain reference
+		FileName fn_gain = mic.getGainFilename();
+		if (fn_gain != prev_gain)
+		{
+			if (isEER)
+				renderer.loadEERGain(fn_gain, Igain());
+			else
+				Igain.read(fn_gain);
+			prev_gain = fn_gain;
+		}
 
 		// Apply gain correction
 		// Probably we can ignore defect correction, because we are not re-aligning.

src/jaz/single_particle/micrograph_handler.cpp
@@ -414,7 +414,9 @@ std::vector<std::vector<Image<Complex>>> MicrographHandler::loadMovie(
 			{
 				if (eer_upsampling < 0)
 					eer_upsampling = micrograph.getEERUpsampling();
-				EERRenderer::loadEERGain(gainFn, lastGainRef(), eer_upsampling);
+				EERRenderer renderer;
+				renderer.read(movieFn, eer_upsampling);
+				renderer.loadEERGain(gainFn, lastGainRef());
 			}
 			else
 			{

src/motioncorr_runner.cpp
@@ -1108,7 +1108,7 @@ bool MotioncorrRunner::executeOwnMotionCorrection(Micrograph &mic) {
 	RCTIC(TIMING_READ_GAIN);
 	if (fn_gain_reference != "") {
 		if (isEER)
-			EERRenderer::loadEERGain(fn_gain_reference, Igain(), eer_upsampling);
+			renderer.loadEERGain(fn_gain_reference, Igain());
 		else
 			Igain.read(fn_gain_reference);
 

src/pipeline_jobs.cpp
@@ -1380,7 +1380,7 @@ void RelionJob::initialiseMotioncorrJob()
 	joboptions["patch_y"] = JobOption("Number of patches Y:", std::string("1"), "Number of patches (in X and Y direction) to apply motioncor2.");
 	joboptions["group_frames"] = JobOption("Group frames:", 1, 1, 5, 1, "Average together this many frames before calculating the beam-induced shifts.");
 	joboptions["bin_factor"] = JobOption("Binning factor:", 1, 1, 2, 1, "Bin the micrographs this much by a windowing operation in the Fourier Tranform. Binning at this level is hard to un-do later on, but may be useful to down-scale super-resolution images. Float-values may be used. Do make sure though that the resulting micrograph size is even.");
-	joboptions["fn_gain_ref"] = JobOption("Gain-reference image:", "", "*.mrc", ".", "Location of the gain-reference file to be applied to the input micrographs. Leave this empty if the movies are already gain-corrected.");
+	joboptions["fn_gain_ref"] = JobOption("Gain-reference image:", "", "*.{mrc,gain}", ".", "Location of the gain-reference file to be applied to the input micrographs. Leave this empty if the movies are already gain-corrected.");
 	joboptions["gain_rot"] = JobOption("Gain rotation:", job_gain_rotation_options, 0, "Rotate the gain reference by this number times 90 degrees clockwise in relion_display. This is the same as -RotGain in MotionCor2. Note that MotionCor2 uses a different convention for rotation so it says 'counter-clockwise'. Valid values are 0, 1, 2 and 3.");
 	joboptions["gain_flip"] = JobOption("Gain flip:", job_gain_flip_options, 0, "Flip the gain reference after rotation. This is the same as -FlipGain in MotionCor2. 0 means do nothing, 1 means flip Y (upside down) and 2 means flip X (left to right).");
 

src/renderEER.cpp
@@ -139,7 +139,7 @@ EERRenderer::EERRenderer()
 	buf = NULL;
 	preread_start = -1;
 	preread_end = -1;
-	eer_upsampling = 2;
+	eer_upsampling = 1;
 }
 
 void EERRenderer::read(FileName _fn_movie, int eer_upsampling)
@@ -212,7 +212,10 @@ void EERRenderer::read(FileName _fn_movie, int eer_upsampling)
 			TIFFGetField(ftiff, EERRenderer::TIFFTAG_EER_RLE_DEPTH, &count, (void*)&rle);
 			rle_bits = *rle;
 			TIFFGetField(ftiff, EERRenderer::TIFFTAG_EER_SUBPIXEL_H_DEPTH, &count, (void*)&subpix_h);
-			TIFFGetField(ftiff, EERRenderer::TIFFTAG_EER_SUBPIXEL_V_DEPTH, &count, (void*)&subpix_v);
+			// Prototype images apparently don't have TIFFTAG_EER_SUBPIXEL_V_DEPTH
+			if (TIFFGetField(ftiff, EERRenderer::TIFFTAG_EER_SUBPIXEL_V_DEPTH, &count, (void*)&subpix_v) == 0)
+				subpix_v = subpix_h;
+
 			if (rle_bits != 7 || *subpix_h != *subpix_v || *subpix_h != 1)
 			{
 				REPORT_ERROR("Unsupported compression scheme: type = " + integerToString(compression) +
@@ -220,6 +223,11 @@ void EERRenderer::read(FileName _fn_movie, int eer_upsampling)
 				             ", subpix_v = " + integerToString(*subpix_v));
 			}
 			subpixel_bits = 1 << *subpix_h;
+
+			if (subpixel_bits == 2 && (eer_upsampling == -1 || eer_upsampling >= 3))
+			{
+				REPORT_ERROR("For subpixel_bits = 2, eer_upsamling must be 1 or 2.");
+			}
 		}
 		else
 			REPORT_ERROR("Unknown compression scheme for EER: " + integerToString(compression));

src/renderEER.h
@@ -89,7 +89,7 @@ class EERRenderer {
 		preread_end = end - 1;
 	}
 
-	void read(FileName _fn_movie, int eer_upsampling=2);
+	void read(FileName _fn_movie, int eer_upsampling=1);
 
 	// Due to a limitation in libtiff (not TIFF specification!),
 	// the maximum number of frames is 65535.
@@ -107,9 +107,13 @@ class EERRenderer {
 
 	// The gain reference for EER is not multiplicative! So the inverse is taken here.
 	// 0 means defect.
+	// This reads the gain reference into the `gain` array but does NOT apply it to movies.
 	template <typename T>
-	static void loadEERGain(FileName fn_gain, MultidimArray<T> &gain, int eer_upsampling)
+	void loadEERGain(FileName fn_gain, MultidimArray<T> &gain)
 	{
+		if (!ready)
+			REPORT_ERROR("EERRenderer::loadEERGain called before ready.");
+
 		const bool is_multiplicative = (fn_gain.getExtension() == "gain");
 		if (is_multiplicative)
 		{
@@ -117,14 +121,14 @@ class EERRenderer {
 			fn_gain += ":tif";
 		}
 
-		// TODO: FIXME: support 2K
-		const int EER_IMAGE_WIDTH = 4096, EER_IMAGE_HEIGHT = 4096;
+		const int detector_width = width;
+		const int detector_height = height;
 
 		Image<T> original;
 		original.read(fn_gain, true, 0, false, true); // explicitly use the first page
 		const int nx_in = XSIZE(original());
 		const int ny_in = YSIZE(original());
-		const long long size_out = EER_IMAGE_WIDTH * eer_upsampling;
+		long long size_out = getWidth();
 
 		// Revert Y flip in TIFF reader
 		if (is_multiplicative)
@@ -142,18 +146,18 @@ class EERRenderer {
 			}
 		} 
 
-		if (eer_upsampling == 2 && nx_in == EER_IMAGE_WIDTH && ny_in == EER_IMAGE_HEIGHT) // gain = 4K and grid = 8K
+		if (eer_upsampling == 2 && nx_in == detector_width && ny_in == detector_height) // (gain=det=4K, grid=8K) or (gain=det=2K, grid=4K)
 		{
 			gain.initZeros(size_out, size_out);
 			FOR_ALL_DIRECT_ELEMENTS_IN_ARRAY2D(gain)
 				DIRECT_A2D_ELEM(gain, i, j) = DIRECT_A2D_ELEM(original(), i / 2, j / 2);
 		}
-		else if ((eer_upsampling == 1 && nx_in == EER_IMAGE_WIDTH && ny_in == EER_IMAGE_HEIGHT) || // gain = 4K and grid = 4K
-		         (eer_upsampling == 2 && nx_in == EER_IMAGE_WIDTH * 2 && ny_in == EER_IMAGE_HEIGHT * 2)) // gain = 8K and grid = 8K
+		else if ((eer_upsampling == 1 && nx_in == detector_width && ny_in == detector_height) || // (gain=det=grid=4K) or (gain=det=grid=2K)
+		         (eer_upsampling == 2 && nx_in == size_out && ny_in == size_out)) // (det=4K, gain=grid=8K) or (det=2K, gain=grid=4K)
 		{
 			gain = original();
 		}
-		else if (eer_upsampling == 1 && nx_in == EER_IMAGE_WIDTH * 2 && ny_in == EER_IMAGE_HEIGHT * 2) // gain = 8K and grid = 4K
+		else if (eer_upsampling == 1 && nx_in == detector_width * 2 && ny_in == detector_height * 2) // (gain=8K, det=grid=4K) or (gain=4K, det=grid=2K)
 		{
 			gain.initZeros(size_out, size_out);
 			FOR_ALL_DIRECT_ELEMENTS_IN_ARRAY2D(original())
@@ -162,7 +166,7 @@ class EERRenderer {
 		else
 		{
 			std::cerr << "Size of input gain: X = " << nx_in << " Y = " << ny_in << " Expected: X = " << size_out << " Y = " << size_out << std::endl;
-			REPORT_ERROR("Invalid gain size in EERRenderer::upsampleEERGain()");
+			REPORT_ERROR("Unsupported gain size in EERRenderer::loadEERGain()");
 		}
 		
 		if (!is_multiplicative)

src/tiff_converter.cpp
@@ -375,7 +375,9 @@ void TIFFConverter::initialise(int _rank, int _total_ranks)
 		{
 			if (fn_gain != "" && rank == 0)
 			{
-				EERRenderer::loadEERGain(fn_gain, gain(), eer_upsampling);
+				EERRenderer renderer;
+				renderer.read(fn_first, eer_upsampling);
+				renderer.loadEERGain(fn_gain, gain());
 				std::cout << "Read an EER gain file " << fn_gain << " NX = " << XSIZE(gain()) << " NY = " << YSIZE(gain()) << std::endl;
 				std::cout << "Taking inverse and re-scaling (when necessary)." << std::endl;
 				gain.write(fn_out + "gain-reference.mrc");

commit 0288290dab6e6cf68cb476a9a81a646d9cc94327

Update movie_reconstruct.cpp
Fixed error introduced in commit 9d0df80 so relion_movie_reconstruct now builds

src/apps/movie_reconstruct.cpp
@@ -317,10 +317,15 @@ void MovieReconstructor::backproject(int rank, int size)
 		if (fn_gain != prev_gain)
 		{
 			if (isEER)
+			{
+				EERRenderer renderer;
 				renderer.loadEERGain(fn_gain, Igain());
+			}
 			else
+			{
 				Igain.read(fn_gain);
 			prev_gain = fn_gain;
+			}
 		}
 
 		// Apply gain correction