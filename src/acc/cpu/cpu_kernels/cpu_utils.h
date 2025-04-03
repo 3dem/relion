4f15fe2e3cd4e43692e5c3263ed447ea7230b6c7
do-jason <jason.do@intel.com>
2025-04-02 22:11:40 -0700

    Refine complex2D and complex3D functions

--- src/acc/cpu/cpu_kernels/cpu_utils.h
+++ src/acc/cpu/cpu_kernels/cpu_utils.h
@@ -121,38 +121,27 @@ size_t ceilfracf(size_t a, size_t b)
 inline
 #endif
 static void complex2D(std::complex<XFLOAT> *mdlComplex, XFLOAT &real, XFLOAT &imag,
-               XFLOAT xp, XFLOAT yp, int mdlX, int mdlInitY)
+				const XFLOAT xp, const XFLOAT yp, const int mdlX, const int mdlInitY)
 {
-	int x0 = floorf(xp);
-	XFLOAT fx = xp - x0;
+	const int x0 = floorf(xp);
+	const XFLOAT fx = xp - x0;
 
-	int y0 = floorf(yp);
-	XFLOAT fy = yp - y0;
-	y0 -= mdlInitY;
+	const int y0 = floorf(yp);
+	const XFLOAT fy = yp - y0;
 
-    size_t offset1 = ((size_t)y0 * (size_t)mdlX + (size_t)x0);
-    size_t offset2 = offset1 + (size_t)1;
-    size_t offset3 = offset1 + (size_t)mdlX;
-    size_t offset4 = offset3 + (size_t)1;  
-    
-	//-----------------------------
-	XFLOAT d00[2], d01[2], d10[2], d11[2];
+	const size_t offset1 = ((size_t)(y0-mdlInitY) * (size_t)mdlX + (size_t)x0);
+	const size_t offset2 = offset1 + (size_t)1;
+	const size_t offset3 = offset1 + (size_t)mdlX;
+	const size_t offset4 = offset3 + (size_t)1;
 
-	d00[0] = mdlComplex[offset1].real();  d00[1] = mdlComplex[offset1].imag();
-	d01[0] = mdlComplex[offset2].real();  d01[1] = mdlComplex[offset2].imag();		    
-	d10[0] = mdlComplex[offset3].real();  d10[1] = mdlComplex[offset3].imag();
-	d11[0] = mdlComplex[offset4].real();  d11[1] = mdlComplex[offset4].imag();
-	
-	//-----------------------------
-	XFLOAT dx0[2], dx1[2];
-	
-	dx0[0] = d00[0] + (d01[0] - d00[0]) * fx;
-	dx1[0] = d10[0] + (d11[0] - d10[0]) * fx;
+	const XFLOAT d00[2] = { mdlComplex[offset1].real(), mdlComplex[offset1].imag() };
+	const XFLOAT d01[2] = { mdlComplex[offset2].real(), mdlComplex[offset2].imag() };
+	const XFLOAT d10[2] = { mdlComplex[offset3].real(), mdlComplex[offset3].imag() };
+	const XFLOAT d11[2] = { mdlComplex[offset4].real(), mdlComplex[offset4].imag() };
 
-	dx0[1] = d00[1] + (d01[1] - d00[1]) * fx;
-	dx1[1] = d10[1] + (d11[1] - d10[1]) * fx;
-	
-	//-----------------------------	
+	const XFLOAT dx0[2] = { d00[0] + (d01[0] - d00[0]) * fx, d00[1] + (d01[1] - d00[1]) * fx };
+	const XFLOAT dx1[2] = { d10[0] + (d11[0] - d10[0]) * fx, d10[1] + (d11[1] - d10[1]) * fx };
+
 	real = dx0[0] + (dx1[0] - dx0[0])*fy;
 	imag = dx0[1] + (dx1[1] - dx0[1])*fy;
 }
@@ -170,65 +159,49 @@ inline
 static void complex3D(
 				std::complex<XFLOAT> * mdlComplex, 
 				XFLOAT &real, XFLOAT &imag,
-				XFLOAT xp, XFLOAT yp, XFLOAT zp, int mdlX, int mdlXY, int mdlInitY, int mdlInitZ
+				const XFLOAT xp, const XFLOAT yp, const XFLOAT zp,
+				const int mdlX, const int mdlXY, const int mdlInitY, const int mdlInitZ
 		)
 {
-	int x0 = floorf(xp);
-	XFLOAT fx = xp - x0;
+	const int x0 = floorf(xp);
+	const XFLOAT fx = xp - x0;
 
-	int y0 = floorf(yp);
-	XFLOAT fy = yp - y0;
-	y0 -= mdlInitY;
+	const int y0 = floorf(yp);
+	const XFLOAT fy = yp - y0;
 
-	int z0 = floorf(zp);
-	XFLOAT fz = zp - z0;
-	z0 -= mdlInitZ;
+	const int z0 = floorf(zp);
+	const XFLOAT fz = zp - z0;
 
-    size_t offset1 = (size_t)((size_t)z0*(size_t)mdlXY+(size_t)y0*(size_t)mdlX+(size_t)x0);
-    size_t offset2 = offset1 + (size_t)1;
-    size_t offset3 = offset1 + (size_t)mdlX;
-    size_t offset4 = offset3 + (size_t)1;
-    size_t offset5 = offset1 + (size_t)mdlXY;
-    size_t offset6 = offset2 + (size_t)mdlXY;
-    size_t offset7 = offset3 + (size_t)mdlXY;
-    size_t offset8 = offset4 + (size_t)mdlXY;    
-        
-	XFLOAT d000[2], d001[2], d010[2], d011[2];
-	XFLOAT d100[2], d101[2], d110[2], d111[2]; 
+	const size_t offset1 = (size_t)((size_t)(z0-mdlInitZ)*(size_t)mdlXY+(size_t)(y0-mdlInitY)*(size_t)mdlX+(size_t)x0);
+	const size_t offset2 = offset1 + (size_t)1;
+	const size_t offset3 = offset1 + (size_t)mdlX;
+	const size_t offset4 = offset3 + (size_t)1;
+	const size_t offset5 = offset1 + (size_t)mdlXY;
+	const size_t offset6 = offset2 + (size_t)mdlXY;
+	const size_t offset7 = offset3 + (size_t)mdlXY;
+	const size_t offset8 = offset4 + (size_t)mdlXY;
 
-	d000[0] = mdlComplex[offset1].real(); d000[1] = mdlComplex[offset1].imag();
-	d001[0] = mdlComplex[offset2].real(); d001[1] = mdlComplex[offset2].imag();    
-	d010[0] = mdlComplex[offset3].real(); d010[1] = mdlComplex[offset3].imag();
-	d011[0] = mdlComplex[offset4].real(); d011[1] = mdlComplex[offset4].imag();
-	d100[0] = mdlComplex[offset5].real(); d100[1] = mdlComplex[offset5].imag();
-	d101[0] = mdlComplex[offset6].real(); d101[1] = mdlComplex[offset6].imag();
-	d110[0] = mdlComplex[offset7].real(); d110[1] = mdlComplex[offset7].imag();
-	d111[0] = mdlComplex[offset8].real(); d111[1] = mdlComplex[offset8].imag();                    
-                                
-	//-----------------------------
-	XFLOAT dx00[2], dx01[2], dx10[2], dx11[2];
-	dx00[0] = d000[0] + (d001[0] - d000[0])*fx;
-	dx01[0] = d100[0] + (d101[0] - d100[0])*fx;
-	dx10[0] = d010[0] + (d011[0] - d010[0])*fx;
-	dx11[0] = d110[0] + (d111[0] - d110[0])*fx;
+	const XFLOAT d000[2] = { mdlComplex[offset1].real(), mdlComplex[offset1].imag() };
+	const XFLOAT d001[2] = { mdlComplex[offset2].real(), mdlComplex[offset2].imag() };
+	const XFLOAT d010[2] = { mdlComplex[offset3].real(), mdlComplex[offset3].imag() };
+	const XFLOAT d011[2] = { mdlComplex[offset4].real(), mdlComplex[offset4].imag() };
+	const XFLOAT d100[2] = { mdlComplex[offset5].real(), mdlComplex[offset5].imag() };
+	const XFLOAT d101[2] = { mdlComplex[offset6].real(), mdlComplex[offset6].imag() };
+	const XFLOAT d110[2] = { mdlComplex[offset7].real(), mdlComplex[offset7].imag() };
+	const XFLOAT d111[2] = { mdlComplex[offset8].real(), mdlComplex[offset8].imag() };
 
-    dx00[1] = d000[1] + (d001[1] - d000[1])*fx;
-    dx01[1] = d100[1] + (d101[1] - d100[1])*fx;
-    dx10[1] = d010[1] + (d011[1] - d010[1])*fx;
-    dx11[1] = d110[1] + (d111[1] - d110[1])*fx;
-	
-	//-----------------------------
-	XFLOAT dxy0[2], dxy1[2];	
-	dxy0[0] = dx00[0] + (dx10[0] - dx00[0])*fy;
-	dxy1[0] = dx01[0] + (dx11[0] - dx01[0])*fy;
-	
-    dxy0[1] = dx00[1] + (dx10[1] - dx00[1])*fy;
-    dxy1[1] = dx01[1] + (dx11[1] - dx01[1])*fy;
+	const XFLOAT dx00[2] = { d000[0] + (d001[0] - d000[0])*fx, d000[1] + (d001[1] - d000[1])*fx };
+	const XFLOAT dx10[2] = { d010[0] + (d011[0] - d010[0])*fx, d010[1] + (d011[1] - d010[1])*fx };
+	const XFLOAT dx01[2] = { d100[0] + (d101[0] - d100[0])*fx, d100[1] + (d101[1] - d100[1])*fx };
+	const XFLOAT dx11[2] = { d110[0] + (d111[0] - d110[0])*fx, d110[1] + (d111[1] - d110[1])*fx };
 
-	//-----------------------------
-	
+	const XFLOAT dxy0[2] = {  dx00[0] + (dx10[0] - dx00[0])*fy,
+								dx00[1] + (dx10[1] - dx00[1])*fy };
+	const XFLOAT dxy1[2] = {  dx01[0] + (dx11[0] - dx01[0])*fy,
+								dx01[1] + (dx11[1] - dx01[1])*fy };
+
 	real = dxy0[0] + (dxy1[0] - dxy0[0])*fz;
-	imag = dxy0[1] + (dxy1[1] - dxy0[1])*fz;	
+	imag = dxy0[1] + (dxy1[1] - dxy0[1])*fz;
 }
 
 } // end of namespace CpuKernels
